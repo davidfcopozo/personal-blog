3fd8813074514e32ecb7d541e3d06ce73a206e12
David Francisco <davidfco.pozo@gmail.com>
2023-06-14 22:49:18 +0200

    Refactored
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/middleware/not-found.ts
+++ src/middleware/not-found.ts
@@ -1,6 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
 
-export const notFound = (req: Request, res: Response) => {
-  return res.status(StatusCodes.NOT_FOUND).send("Route does not exist");
+const notFound = (_req: Request, res: Response) => {
+  res.status(StatusCodes.NOT_FOUND).send("Route does not exist");
 };
+
+module.exports = notFound;
