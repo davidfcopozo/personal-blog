e60aeb700033d731daa83797f72fbaa09d5e327a
David Francisco <davidfco.pozo@gmail.com>
2023-06-14 22:50:36 +0200

    router created
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  verifyEmail,
+  login,
+  forgotPassword,
+  resetPassword,
+  logout,
+} = require("../controllers/authController");
+
+router.route("/register").post(register);
+router.route("/verify-email").post(verifyEmail);
+router.route("/login").post(login);
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password").post(resetPassword);
+router.route("/logout").get(logout);
+
+export = router;
