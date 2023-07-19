1fa5b254bb3e240295479f3cc115becb6256c4b6
David Francisco <davidfco.pozo@gmail.com>
2023-07-19 23:06:06 +0200

    Conditional chain added
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -11,20 +11,20 @@ type errorHandlerMiddlewareProps = {
 const errorHandlerMiddleware = ({ err, res }: errorHandlerMiddlewareProps) => {
   let customError = {
     //Set default
-    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || "Something went wrong, please try again later",
+    statusCode: err?.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err?.message || "Something went wrong, please try again later",
   };
 
   //Check if the error is about email or password validation which name of the mongoose err object is 'ValidationError
-  if (err.name === "ValidationError") {
+  if (err?.name === "ValidationError") {
     customError.msg = Object.values(err.errors)
-      .map((item) => (item as Error).message)
+      .map((item) => (item as Error)?.message)
       .join(", ");
 
     customError.statusCode = 400;
   }
 
-  if (err.code && err.code === 11000) {
+  if (err?.code && err.code === 11000) {
     //If the code number of the mongoose err object is equal to 11000 (meaning there is a duplicate value)
     //Modify the following properties from the customError object
     customError.msg = `Duplicate value enter for the ${Object.keys(
@@ -34,7 +34,7 @@ const errorHandlerMiddleware = ({ err, res }: errorHandlerMiddlewareProps) => {
   }
 
   //Check if the error is about invalid job id which name of the mongoose err object is "CastError"
-  if (err.name === "CastError") {
+  if (err?.name === "CastError") {
     customError.msg = `Not item found with id: ${err.value}`;
     customError.statusCode = 404;
   }
