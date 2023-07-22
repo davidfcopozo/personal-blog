cba7d2e44bf33356337d99fb3beebcc0a486f243
David Francisco <davidfco.pozo@gmail.com>
2023-07-22 22:09:05 +0200

    Centralized routes
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+const authRouter = require("./authRoutes");
+const postRouter = require("./postRoutes");
+const commentRoutes = require("./commentRoutes");
+
+const router = Router();
+
+/*
+app.use("/api/v1/users", userRouter); 
+*/
+
+router.use("/api/v1/auth", authRouter);
+router.use("/api/v1/comments", commentRoutes);
+router.use("/api/v1/posts", postRouter);
+
+module.exports = router;
