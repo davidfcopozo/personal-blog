c84b3b9f0310eaa888f384165c4a74b710fbabcd
David Francisco <davidfco.pozo@gmail.com>
2023-07-31 23:06:17 +0200

    Reply routes added
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const authRouter = require("./authRoutes");
 const postRouter = require("./postRoutes");
 const commentRoutes = require("./commentRoutes");
+const replyRoutes = require("./replyRoutes");
 
 const router = Router();
 
@@ -11,6 +12,7 @@ app.use("/api/v1/users", userRouter);
 
 router.use("/api/v1/auth", authRouter);
 router.use("/api/v1/comments", commentRoutes);
+router.use("/api/v1/replies", replyRoutes);
 router.use("/api/v1/posts", postRouter);
 
 module.exports = router;
