2fd3b2de570a5f73b6eeb8a0c3244978441cdca0
David Francisco <davidfco.pozo@gmail.com>
2023-08-04 23:57:04 +0200

    Reply model removed
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/models/replyModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const replySchema = new mongoose.Schema({
-  postedBy: { type: mongoose.Schema.Types.ObjectId },
-  comment: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
-  content: { type: String, require: [true, "Reply can't be empty"] },
-});
-
-module.exports = mongoose.model("Reply", replySchema);
