4819ee4147987fa86844f7887ab8bb60aea34740
David Francisco <davidfco.pozo@gmail.com>
2023-08-18 19:09:10 +0200

    OverwriteErrorModel fixed
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/models/postModel.ts
+++ src/models/postModel.ts
@@ -18,4 +18,4 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Post", postSchema);
+module.exports = mongoose.models.Post || mongoose.model("Post", postSchema);
