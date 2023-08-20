25323c0379e04a7c926709579e27065a0f3e58d6
David Francisco <davidfco.pozo@gmail.com>
2023-08-20 18:18:23 +0200

    Update user route added

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-//const auth = require("../middleware/auth");
-const { getUsers, getUserById } = require("../controllers/userController");
+const auth = require("../middleware/auth");
+const {
+  getUsers,
+  getUserById,
+  updateUserById,
+} = require("../controllers/userController");
 
 router.route("/").get(getUsers);
-router.route("/:id").get(getUserById);
+router.route("/:id").get(getUserById).patch(auth, updateUserById);
 
 export default router;
