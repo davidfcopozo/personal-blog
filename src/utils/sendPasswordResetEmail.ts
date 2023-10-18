b60a0f0d4c8778cf6620e91594e99c7082b98115
David Francisco <davidfco.pozo@gmail.com>
2023-10-18 12:01:31 +0200

    Changed name of the blog
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/utils/sendPasswordResetEmail.ts
+++ src/utils/sendPasswordResetEmail.ts
@@ -16,7 +16,7 @@ export const sendPasswordResetEmail = async ({
     subject: "Password Reset",
     html: `
     <h4>Hello ${firstName}</h4>
-    <p>You recently requested to reset the password for your Indid Coding account. You can reset your password by clicking the link below:</p>
+    <p>You recently requested to reset the password for your DevStacks account. You can reset your password by clicking the link below:</p>
     <p>Click <a href="${verificationUrl}">here</a> to reset your password.</p>
     <p>No changes have been made to your account yet.</p>
     <p>If you did not request a password reset, please ignore this email or reply to let us know. This password reset link is only valid for the next 30 minutes.</p>
