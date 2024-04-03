6ed67aa3031036df783fda4cb12a35a18d2b2290
David Francisco <davidfco.pozo@gmail.com>
2024-04-03 22:48:49 +0200

    Files copy from the api folder to create client directory removed
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HFNYSdhMuvkH0TlqaFaX08MQi6l4V+m8Lh9cjWZtwO8=\"\n}"
--- client/src/models/custom-error.model.ts
+++
@@ -1,9 +0,0 @@
-export class CustomError {
-  msg!: string;
-  statusCode!: number;
-
-  constructor(message: string, status: number = 500) {
-    this.msg = message;
-    this.statusCode = status;
-  }
-}
