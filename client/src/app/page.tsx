017011c58eb7fc4f3e46201009ddd633c6b14004
David <davidfco.pozo@gmail.com>
2024-05-29 23:25:07 +0200

    Blog card basic structure added
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,19 @@
-
+import { BlogCard } from "@/components/blog-card";
 
 export default function Home() {
   return (
     <div className="container p-2 mx-auto">
       <div className="flex flex-row flex-wrap p-2 sm:p-4">
-        <main role="main" className="w-full mt-12 sm:w-2/3 md:w-3/4 pt-1 px-2">
+        <main
+          role="main"
+          className="w-full flex flex-wrap gap-4 mt-12 sm:w-2/3 md:w-3/4 pt-1 px-2"
+        >
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
         </main>
         <aside className="w-full hidden  pt-12 sm:flex sm:w-1/3 md:w-1/4 px-2 border-l-2 border-secondary">
-          <div className="sticky top-16 p-4 bg-background rounded-xl w-full h-[84vh]">
-
-          </div>
+          <div className="sticky top-16 p-4 bg-background rounded-xl w-full h-[84vh]"></div>
         </aside>
       </div>
     </div>
