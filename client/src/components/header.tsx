9ab26fe7cec55b14072ba3766cf134147b37d7b6
David <davidfco.pozo@gmail.com>
2024-05-31 17:20:18 +0200

    refactor: Update header component styles and structure

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -23,13 +23,15 @@ export function Header() {
     theme === "system" && systemTheme === "dark"
       ? "#ffffff"
       : theme === "system" && systemTheme === "light"
-        ? "#000000" : theme === "light"
-          ? "#000000"
-          : theme === "light"
-            ? "#000000" : "#ffffff";
+      ? "#000000"
+      : theme === "light"
+      ? "#000000"
+      : theme === "light"
+      ? "#000000"
+      : "#ffffff";
 
   return (
-    <header className="fixed w-full justify-between top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="fixed w-full justify-between top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <Link
         href="#"
         className="flex items-center gap-2 text-lg font-semibold md:text-base"
@@ -48,24 +50,20 @@ export function Header() {
         </div>
       </form>
       <nav className="ml-auto flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-end md:gap-5 md:text-sm lg:gap-6">
-        <div className="flex text-sm gap-3 items-center md:gap-5 lg:gap-6">
-          <Link
+        <div className="flex text-sm gap-2 items-center md:gap-4 lg:gap-5">
+          {/*       <Link
             href="#"
             className="text-foreground transition-colors hover:text-foreground"
           >
             Write
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Sign In
-          </Link>
+          </Link> */}
+
           <ModeToggle />
         </div>
         <div
-          className={`${true ? "md:hidden" : "md:hidden"
-            } hidden md:flex md:gap-5 lg:gap-6`}
+          className={`${
+            true ? "md:hidden" : "md:hidden"
+          } hidden md:flex md:gap-5 lg:gap-6`}
         >
           <Link
             href="#"
@@ -89,6 +87,12 @@ export function Header() {
       </nav>
 
       <div className="flex items-center gap-4 md:gap-2 lg:gap-4">
+        <Link
+          href="#"
+          className="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          Sign In
+        </Link>
         {/*           <form className="ml-auto flex-1 flex-initial md:hidden">
             <div className="relative">
               <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
