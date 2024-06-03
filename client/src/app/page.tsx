aac010fa3a0273eb1267d89bd63ac0dc6f0b1d90
David <davidfco.pozo@gmail.com>
2024-06-03 20:03:53 +0200

    feat: Add search functionality to sidebar
    Added search functionality to the sidebar in the Home page.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,6 @@
 import { BlogCard } from "@/components/blog-card";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 
 export default function Home() {
   return (
@@ -12,8 +14,19 @@ export default function Home() {
           <BlogCard />
           <BlogCard />
         </main>
-        <aside className="w-full hidden  pt-12 sm:flex sm:w-1/3 md:w-1/4 px-2 border-l-2 border-secondary">
-          <div className="sticky top-16 p-4 bg-background rounded-xl w-full h-[84vh]"></div>
+        <aside className="w-full hidden pt-12 sm:flex sm:flex-column sm:w-1/3 md:w-1/4 px-2 border-l-2 border-secondary">
+          <div className="sticky top-16 p-4 bg-background rounded-xl w-full h-[84vh]">
+            <form className="ml-auto flex-1 sm:flex-start">
+              <div className="relative">
+                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="search"
+                  placeholder="Search posts..."
+                  className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+                />
+              </div>
+            </form>
+          </div>
         </aside>
       </div>
     </div>
