2b32a6507f18d5115fd6fb1c1d4eef8eddde57c7
David <davidfco.pozo@gmail.com>
2024-06-07 19:55:06 +0200

    feat: Add register page
    Added a new register page component with a signup form.

---
+++ client/src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import { SignupForm } from "@/components/register-form";
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="flex mt-16 text-foreground h-full">
+      <SignupForm />
+    </div>
+  );
+};
+
+export default Register;
