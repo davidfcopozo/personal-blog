b64c5e2a364710b3390953d5f5c0598ac3395a6e
David <davidfco.pozo@gmail.com>
2024-06-07 19:54:13 +0200

    feat: Add login page
    Added a new login page component with a login form.

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/login-form";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex mt-16 text-foreground h-full">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
