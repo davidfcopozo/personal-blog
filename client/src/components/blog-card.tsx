0ff85334e69d2588f811516d671c2b425b0e5aed
David <davidfco.pozo@gmail.com>
2024-06-14 21:32:34 +0200

    Refactor BlogCard component to use Avatar component for displaying user avatars

--- client/src/components/blog-card.tsx
+++ client/src/components/blog-card.tsx
@@ -1,8 +1,21 @@
+import {
+  calculateReadingTime,
+  getFullName,
+  getNameInitials,
+  showMonthDay,
+  truncateText,
+} from "@/lib/utils";
 import { Bookmark, Clock, MessageCircle, ThumbsUp } from "lucide-react";
-import Image from "next/image";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import Link from "next/link";
 
-export const BlogCard = () => {
+interface BlogPostProps {
+  post: any;
+  user: any;
+}
+
+export const BlogCard: React.FC<BlogPostProps> = ({ post, user }) => {
+  const { title, content, createdAt, bookmarks, likes, comments } = post;
   return (
     <div className="max-w-sm w-full rounded-b-lg border-b rounded-b-lg transition-all duration-300 lg:max-w-full lg:flex lg:border-x-0 lg:border-secondary lg:rounded-b-none hover:scale-[1.02] lg:hover:rounded-b hover:shadow-[0px_1px_0px_0px] hover:shadow-muted-foreground">
       <Link
@@ -15,17 +28,15 @@ export const BlogCard = () => {
         <div className="mb-8">
           <p className="text-sm gap-1 text-center text-muted-foreground flex items-center pb-2">
             <Clock size={16} strokeWidth={2} />
-            15 min read
+            {calculateReadingTime(content)}
           </p>
           <Link href="#" legacyBehavior>
             <a>
               <h2 className="text-foreground font-bold text-xl mb-2">
-                Can coffee make you a better developer?
+                {title}
               </h2>
               <p className="text-foreground text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                exercitationem praesentium nihil.
+                {truncateText(content, 150)}
               </p>
             </a>
           </Link>
@@ -33,36 +44,33 @@ export const BlogCard = () => {
         <div className="flex justify-between items-center">
           <div className="flex items-center">
             <Link href="/#">
-              <Image
-                className="w-10 h-10 rounded-full mr-4"
-                src="/TechyComm-Logo.svg"
-                alt="Avatar of Jonathan Reinink"
-                width={300}
-                height={200}
-              />
+              <Avatar className="w-10 h-10 border  mr-4">
+                <AvatarImage src={user?.avatar} />
+                <AvatarFallback>{getNameInitials(user)}</AvatarFallback>
+              </Avatar>
             </Link>
             <div className="text-sm">
               <Link href="/#" legacyBehavior>
                 <a className="text-muted-foreground font-semibold leading-none transition-all duration-300 hover:text-foreground">
-                  David Francisco
+                  {getFullName(user)}
                 </a>
               </Link>
-              <p className="text-muted-foreground">Aug 18</p>
+              <p className="text-muted-foreground">{showMonthDay(createdAt)}</p>
             </div>
           </div>
           <div className="flex gap-2 text-muted-foreground">
             <div className="flex justify-center transition-all duration-300 cursor-pointer hover:text-foreground">
               <Bookmark size={18} strokeWidth={2} />
-              <span className="text-sm">5</span>
+              <span className="text-sm pl-[0.2em]">{bookmarks?.length}</span>
             </div>
             <div className="flex justify-center transition-all duration-300 cursor-pointer hover:text-foreground">
               <ThumbsUp size={18} strokeWidth={2} />
-              <span className="text-sm">15</span>
+              <span className="text-sm pl-[0.2em]">{likes?.length}</span>
             </div>
             <Link href="#" legacyBehavior>
               <a className="flex justify-center transition-all duration-300 hover:text-foreground">
                 <MessageCircle size={18} strokeWidth={2} />
-                <span className="text-sm">502</span>
+                <span className="text-sm pl-[0.2em]">{comments?.length}</span>
               </a>
             </Link>
           </div>
