d275753e9a1dc9f2ba56fed1a809d7b5a4fa4077
David <davidfco.pozo@gmail.com>
2024-06-25 20:04:14 +0200

    feat: Update global.d.ts typings for next-auth and mongodb
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/global.d.ts
@@ -0,0 +1,32 @@
+import type { MongoClient } from "mongodb";
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      email: string;
+      role: string;
+      token: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    role: string;
+    accessToken: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role: string;
+    token: string;
+  }
+}
+
+declare global {
+  namespace globalThis {
+    var _mongoClientPromise: Promise<MongoClient>;
+  }
+}
