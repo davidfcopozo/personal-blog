6a1d9a1b3fe940915d097948a652b14dae1d91ff
David <davidfco.pozo@gmail.com>
2024-06-26 19:49:40 +0200

    feat: Add form credentials submission logic to login form and toaster for errors
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -10,58 +10,97 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useToast } from "./ui/use-toast";
 
 export function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const route = useRouter();
+  const { toast } = useToast();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    }).then((res) => {
+      e.preventDefault();
+      if (!res?.ok && res?.status !== 200) {
+        toast({
+          variant: "destructive",
+          title: "Login Failed",
+          description: res?.error || `Login failed. Please try again.`,
+        });
+        return;
+      }
+      route.push("/dashboard");
+    });
+  };
   return (
     <Card className="mx-auto max-w-sm mt-8 mb-4 sm:mb-4 ">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
+      <form onSubmit={handleSubmit}>
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-          <div className="flex flex-col text-center gap-2">
-            <p>Or</p>
-            <Button variant="outline" className="w-full">
-              Login with GitHub
-              <span className="sr-only">Sign up with Google GitHub</span>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="#"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input
+                id="password"
+                type="password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
             </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-              <span className="sr-only">Sign up with Google button</span>
-            </Button>
+            <div className="flex flex-col text-center gap-2">
+              <p>Or</p>
+              <Button variant="outline" className="w-full">
+                Login with GitHub
+                <span className="sr-only">Sign up with Google GitHub</span>
+              </Button>
+              <Button variant="outline" className="w-full">
+                Login with Google
+                <span className="sr-only">Sign up with Google button</span>
+              </Button>
+            </div>
           </div>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
-            Sign up
-          </Link>
-        </div>
-      </CardContent>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </CardContent>
+      </form>
     </Card>
   );
 }
