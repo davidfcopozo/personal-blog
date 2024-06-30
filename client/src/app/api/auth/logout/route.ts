aa2727edd3903ccbc2c069b9836ef2f2ab871bee
David <davidfco.pozo@gmail.com>
2024-06-30 16:20:27 +0200

    refactor: Add logout route implementation
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/app/api/auth/logout/route.ts
@@ -0,0 +1,51 @@
+// auth/logout/route.ts
+
+import axios from "axios";
+import { getToken } from "next-auth/jwt";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const token = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    return new Response(JSON.stringify({ message: "No token found" }), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  try {
+    const res = await axios.get(
+      `${process.env.BACKEND_API_ENDPOINT}/auth/logout`,
+      {
+        headers: {
+          Authorization: `Bearer ${token.accessToken}`,
+        },
+      }
+    );
+
+    console.log("RESPONSE FROM AUTH/LOGOUT", JSON.stringify(res.data));
+
+    return new Response(JSON.stringify(res.data), {
+      status: res.status,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: any) {
+    console.error(
+      "ERROR FROM AUTH/LOGOUT",
+      error.response?.data?.msg || error.message
+    );
+    return new Response(
+      JSON.stringify({
+        message: error.response?.data?.msg || "Internal server error",
+      }),
+      {
+        status: error.response?.status || 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
