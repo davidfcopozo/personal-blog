46646c660d8e3e4e06cc9e5d66239048f51c7de9
David <davidfco.pozo@gmail.com>
2024-06-30 16:15:20 +0200

    refactor: Update auth middleware to handle token from both cookie and header
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -11,24 +11,26 @@ export const auth = (
   next: NextFunction
 ) => {
   try {
-    const authCookie = req.signedCookies;
+    let token;
 
-    if (!authCookie) {
-      throw new Unauthenticated("Unauthorized: No cookie provided");
+    const authCookie = req.signedCookies;
+    if (authCookie && authCookie.token) {
+      token = authCookie.token;
     }
 
-    const token = authCookie.token;
+    const authHeader = req.headers.authorization;
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
+    }
 
     if (!token) {
       throw new Unauthenticated("Unauthorized: No token provided");
     }
 
     const validToken: JwtPayload | string | boolean = isTokenValid(token);
-
     if (!validToken) {
       throw new Unauthenticated("Unauthorized: Invalid token");
     }
-
     req.user = validToken;
     next();
   } catch (err) {
