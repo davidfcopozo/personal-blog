8381df82234d48acba2ce321a15e4197cb5340bd
David <davidfco.pozo@gmail.com>
2024-07-03 16:31:33 +0200

    refactor: Add GET route for retrieving user data by ID
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/app/api/users/[id]/route.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  if (!id) {
+    return new Response(
+      JSON.stringify({
+        message: "User does't exist, has been deleted or ID is incorrect",
+      }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  try {
+    const res = await axios.get(
+      `${process.env.BACKEND_API_ENDPOINT}/users/${id}`
+    );
+
+    return new Response(JSON.stringify(res.data), {
+      status: res.status,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: any) {
+    return new Response(
+      JSON.stringify({
+        message: error.response?.data?.msg || "Internal server error",
+      }),
+      {
+        status: error.response?.status || 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
