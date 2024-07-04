b06e64ddc1cecbf8521ad8d3bf34c752a9b62976
David <davidfco.pozo@gmail.com>
2024-07-04 22:31:17 +0200

    feat: Add BouncingCirclesLoader component
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/components/bouncing-squares-loader.tsx
@@ -0,0 +1,11 @@
+import { BouncingCircles } from "./icons";
+
+export default function BouncingCirclesLoader() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 transition-all duration-200">
+      <div className="flex items-center justify-center space-x-2">
+        <BouncingCircles />
+      </div>
+    </div>
+  );
+}
