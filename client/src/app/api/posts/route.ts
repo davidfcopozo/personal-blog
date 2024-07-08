9ceb0a37867ae84fb65c2d85b197be380edcb4fd
David <davidfco.pozo@gmail.com>
2024-07-08 18:07:36 +0200

    refactor: Add GET function to fetch posts from API
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/app/api/posts/route.ts
@@ -0,0 +1,26 @@
+export async function GET() {
+  try {
+    const res = await fetch(`${process.env.BACKEND_API_ENDPOINT}/posts`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+
+    const data = await res.json();
+
+    return new Response(JSON.stringify(data), {
+      status: res.status,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: any) {
+    return new Response(
+      JSON.stringify({
+        message: error.response?.data?.msg || "Internal server error",
+      }),
+      {
+        status: error.response?.status || 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
