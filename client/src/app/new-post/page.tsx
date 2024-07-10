f7b33adda9ff6dd5cc5a8ac7eb2498f3312f6078
David <davidfco.pozo@gmail.com>
2024-07-10 12:13:06 +0200

    feat: Import BlogEditor blog to the new post page
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/new-post/page.tsx
+++ client/src/app/new-post/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+import BlogEditor from "@/components/blog-editor";
+import React, { useMemo, useState } from "react";
+
+const NewPostPage = () => {
   const [content, setContent] = useState("");
 
   const handleContentChange = (data: { title: string; content: string }) => {
@@ -13,3 +18,13 @@
     console.log("Submitting blog post with content:", content);
   };
 
+  return (
+    <div className="mt-24">
+      <form onSubmit={handleSubmit}>
+        <BlogEditor initialValue={content} onSave={handleContentChange} />
+      </form>
+    </div>
+  );
+};
+
+export default NewPostPage;
