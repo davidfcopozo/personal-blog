0b06cde8c0a0bb79d531cd9b302db91827cf5827
David <davidfco.pozo@gmail.com>
2024-07-14 18:29:28 +0200

    feat: Add usePostRequest hook for making POST requests
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/hooks/usePostRequest.ts
@@ -0,0 +1,28 @@
+import { usePostRequestType } from "@/typings/types";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+const baseULR = "http://localhost:3000";
+
+function usePostRequest({
+  url,
+  onSuccess,
+  onError,
+  onMutate,
+}: usePostRequestType) {
+  const mutateFunction = async (body: any) => {
+    const response = await axios.post(`${baseULR}/${url}`, body);
+    return response.data;
+  };
+
+  const { mutate, data, status, error } = useMutation({
+    mutationFn: mutateFunction,
+    onSuccess: onSuccess,
+    onError: onError,
+    onMutate: onMutate,
+  });
+
+  return { mutate, data, status, error };
+}
+
+export default usePostRequest;
