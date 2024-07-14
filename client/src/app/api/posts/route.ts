65baf5890794392bafbd8e4ced9ff6b482bcd8d3
David <davidfco.pozo@gmail.com>
2024-07-14 18:23:14 +0200

    feat: Add POST function to handle creating new posts
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/api/posts/route.ts
+++ client/src/app/api/posts/route.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export async function GET() {
   try {
     const res = await fetch(`${process.env.BACKEND_API_ENDPOINT}/posts`);
@@ -13,6 +15,35 @@ export async function GET() {
       headers: { "Content-Type": "application/json" },
     });
   } catch (error: any) {
+    return new Response(
+      JSON.stringify({
+        message: error.response?.data?.msg || "Internal server error",
+      }),
+      {
+        status: error.response?.status || 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const res: Response = await axios.post(
+      `${process.env.BACKEND_API_ENDPOINT}/posts`,
+      body
+    );
+
+    if (!res.ok) {
+      throw new Error(res?.statusText);
+    }
+
+    return new Response(JSON.stringify(await res.json()), {
+      status: res.status,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: Error | any) {
     return new Response(
       JSON.stringify({
         message: error.response?.data?.msg || "Internal server error",
