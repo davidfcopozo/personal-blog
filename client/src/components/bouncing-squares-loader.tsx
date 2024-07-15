ae63f990515251e943ed1f30ad004ece58e7fcb6
David <davidfco.pozo@gmail.com>
2024-07-15 15:51:42 +0200

    refactor: Update BouncingCirclesLoader component to use background color from theme
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/bouncing-squares-loader.tsx
+++ client/src/components/bouncing-squares-loader.tsx
@@ -2,7 +2,7 @@ import { BouncingCircles } from "./icons";
 
 export default function BouncingCirclesLoader() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 transition-all duration-200">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-background transition-all duration-200">
       <div className="flex items-center justify-center space-x-2">
         <BouncingCircles />
       </div>
