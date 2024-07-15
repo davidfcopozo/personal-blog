572564cfb272fac4cf738615608c0c30558a5a3a
David <davidfco.pozo@gmail.com>
2024-07-15 10:39:49 +0200

    refactor: Update ExtendedEditor interface to include additional properties
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/typings/interfaces/index.ts
+++ client/src/typings/interfaces/index.ts
@@ -1,6 +1,13 @@
+import { Editor } from "tinymce";
+
 export interface BlogEditorProps {
   onSave: (
     e: React.FormEvent,
     data: { title: string; content: string }
   ) => void;
+}
+
+export interface ExtendedEditor extends Editor {
+  dom: any;
+  iframeElement: HTMLIFrameElement; // Assuming iframeElement is of type HTMLIFrameElement
 }
