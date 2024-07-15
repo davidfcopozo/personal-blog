e9f6a9037761acf5032d36e28894131a9466b7fe
David <davidfco.pozo@gmail.com>
2024-07-15 15:51:02 +0200

    refactor: Update BlogEditor to handle loading state and fix color theme flickering bug
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/blog-editor.tsx
+++ client/src/components/blog-editor.tsx
@@ -15,12 +15,16 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import "@/styles/tinyCME-styles.css";
 import { useTheme } from "next-themes";
+import EditorWrapper from "./editor-wrapper";
+import BouncingCirclesLoader from "./bouncing-squares-loader";
 
 export default function BlogEditor({ onSave }: BlogEditorProps) {
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
   const [currentImages, setCurrentImages] = useState<string[]>([]);
   const [darkTheme, setDarkTheme] = useState(editorColors.dark);
+  const [isEditorReady, setIsEditorReady] = useState(false);
+  const [isThemeReady, setIsThemeReady] = useState(false);
   const editorRef = useRef<any>(null);
   const { toast } = useToast();
   const { theme, systemTheme } = useTheme();
@@ -31,6 +35,7 @@ export default function BlogEditor({ onSave }: BlogEditorProps) {
         ? editorColors.dark
         : editorColors.light;
     setDarkTheme(newTheme);
+    setIsThemeReady(true);
   }, [theme, systemTheme]);
 
   useEffect(() => {
@@ -147,84 +152,87 @@ export default function BlogEditor({ onSave }: BlogEditorProps) {
       className="blog-editor px-4 bg-background"
       style={darkTheme as React.CSSProperties}
     >
-      <div className="flex gap-4 px-2 mx-auto pb-4">
-        <Input
-          type="text"
-          placeholder="Enter blog  title"
-          className="text-2xl font-bold"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <div className="flex gap-2">
-          <Button
-            variant="outline"
-            className="bg-background"
-            onClick={(e) => handleSave(e)}
-          >
-            Save Draft
-          </Button>
-          <Button className="bg-foreground" onClick={(e) => handleSave(e)}>
-            Publish
-          </Button>
+      {!isEditorReady && <BouncingCirclesLoader />}
+      <EditorWrapper isReady={isEditorReady && isThemeReady}>
+        <div className="flex gap-4 px-2 mx-auto pb-4">
+          <Input
+            type="text"
+            placeholder="Enter blog  title"
+            className="text-2xl font-bold"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div className="flex gap-2">
+            <Button
+              variant="outline"
+              className="bg-background"
+              onClick={(e) => handleSave(e)}
+            >
+              Save Draft
+            </Button>
+            <Button className="bg-foreground" onClick={(e) => handleSave(e)}>
+              Publish
+            </Button>
+          </div>
         </div>
-      </div>
-      {/* 
-      </div> */}
-      <Editor
-        onInit={(evt, editor) => {
-          editorRef.current = editor;
-        }}
-        value={content}
-        apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY}
-        toolbar={true}
-        init={{
-          height: 500,
-          menubar: true,
-          plugins: [
-            "advlist",
-            "autolink",
-            "lists",
-            "link",
-            "image",
-            "charmap",
-            "preview",
-            "anchor",
-            "searchreplace",
-            "visualblocks",
-            "code",
-            "fullscreen",
-            "insertdatetime",
-            "table",
-            "code",
-            "help",
-            "wordcount",
-          ],
-          toolbar:
-            "undo redo | blocks | " +
-            "bold italic forecolor | alignleft aligncenter " +
-            "alignright alignjustify | bullist numlist outdent indent | " +
-            "removeformat | image | help",
-          setup: (editor) => {
+        <Editor
+          onInit={(evt, editor) => {
             editorRef.current = editor;
-            editor.on("init", () =>
-              updateEditorTheme(editor as any, darkTheme)
-            );
-          },
-          content_style: `
+            setIsEditorReady(true);
+          }}
+          value={content}
+          apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY}
+          toolbar={true}
+          init={{
+            height: 500,
+            menubar: true,
+            plugins: [
+              "advlist",
+              "autolink",
+              "lists",
+              "link",
+              "image",
+              "charmap",
+              "preview",
+              "anchor",
+              "searchreplace",
+              "visualblocks",
+              "code",
+              "fullscreen",
+              "insertdatetime",
+              "table",
+              "code",
+              "help",
+              "wordcount",
+            ],
+            toolbar:
+              "undo redo | blocks | " +
+              "bold italic forecolor | alignleft aligncenter " +
+              "alignright alignjustify | bullist numlist outdent indent | " +
+              "removeformat | image | help",
+            setup: (editor) => {
+              editorRef.current = editor;
+              editor.on("init", () => {
+                updateEditorTheme(editor as any, darkTheme);
+                setTimeout(() => setIsEditorReady(true), 800);
+              });
+            },
+            content_style: `
               body {
                 font-family: Helvetica, Arial, sans-serif;
                 font-size: 14px;
               }
             `,
-          body_class: "mce-content-body",
-          content_css: false, // Disable default content CSS
-          images_upload_handler: handleImageUpload,
-          file_picker_types: "image",
-          automatic_uploads: true,
-          file_picker_callback: handleFilePicker,
-        }}
-        onEditorChange={handleEditorChange}
-      />
+            body_class: "mce-content-body",
+            content_css: false, // Disable default content CSS
+            images_upload_handler: handleImageUpload,
+            file_picker_types: "image",
+            automatic_uploads: true,
+            file_picker_callback: handleFilePicker,
+          }}
+          onEditorChange={handleEditorChange}
+        />
+      </EditorWrapper>
     </div>
   );
 }
