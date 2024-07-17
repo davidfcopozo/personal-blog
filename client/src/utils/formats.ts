f9ad269fe190e08b36e24053a27316da2866343f
David <davidfco.pozo@gmail.com>
2024-07-17 13:02:25 +0200

    refactor: Move cn function from formats.ts to lib/utils.ts
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/utils/formats.ts
+++ client/src/utils/formats.ts
@@ -1,10 +1,3 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 export function getRelativeTime(
   date: Date | number,
   language: "en" | "es" = "en"
