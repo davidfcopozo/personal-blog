592a0b5a6cffac4b61ffa99bb20574b21ae580aa
David <davidfco.pozo@gmail.com>
2024-07-25 19:37:30 +0200

    refactor: Update NewPostPage component to remove unused code and simplify structure
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/new-post/page.tsx
+++ client/src/app/new-post/page.tsx
@@ -1,30 +1,13 @@
 "use client";
+import React from "react";
 import BlogEditor from "@/components/blog-editor";
-import React, { useMemo, useState } from "react";
 
 const NewPostPage = () => {
-  const handleSubmit = (
-    e: React.FormEvent,
-    data: { title: any; content: any }
-  ) => {
-    try {
-      const { title, content } = data;
-      e.preventDefault();
-      /*       if (!title) {
-        return;
-      } */
-
-      console.log("Submitting blog post with content:", content);
-    } catch (error: Error | any) {
-      console.log("error===>", error.message);
-    }
-  };
-
   return (
     <div className="pt-24 bg-background min-h-screen">
-      <form className="bg-background">
-        <BlogEditor onSave={handleSubmit} />
-      </form>
+      <div className="bg-background">
+        <BlogEditor />
+      </div>
     </div>
   );
 };
