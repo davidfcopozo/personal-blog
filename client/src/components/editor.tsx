ce0d368765b7585116f045d8c1ffc40ec013b5c5
David <davidfco.pozo@gmail.com>
2024-07-26 20:41:51 +0200

    refactor: Update Editor component to insert image at correct position
    Add key to editor
    
    Add display name to editor
    
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/editor.tsx
+++ client/src/components/editor.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useCallback, useMemo } from "react";
+import React, { forwardRef, RefObject, useCallback, useMemo } from "react";
 import ReactQuill from "react-quill";
 import { formats } from "@/utils/blog-editor";
 import { EditorProps } from "@/typings/interfaces";
@@ -17,13 +17,15 @@ const Editor = forwardRef<ReactQuill | null, EditorProps>((props, ref) => {
         const file = input.files[0];
         try {
           const url = await handleImageUpload(file);
-          const quill = (
-            ref as React.RefObject<ReactQuill>
-          ).current?.getEditor();
-          if (quill) {
+          const quillRef = ref as RefObject<ReactQuill>;
+
+          if (quillRef.current) {
+            const quill = quillRef.current.getEditor();
             const range = quill.getSelection(true);
             quill.insertEmbed(range.index, "image", url);
-            quill.setSelection(range.index, 1);
+            quill.setSelection(range.index + 1, 0);
+          } else {
+            console.error("Quill instance not found");
           }
         } catch (error) {
           console.error("Error uploading image:", error);
@@ -61,6 +63,7 @@ const Editor = forwardRef<ReactQuill | null, EditorProps>((props, ref) => {
 
   return (
     <ReactQuill
+      key="quil-editor"
       ref={ref}
       className="h-screen w-full"
       formats={formats}
@@ -70,5 +73,7 @@ const Editor = forwardRef<ReactQuill | null, EditorProps>((props, ref) => {
     />
   );
 });
+
+Editor.displayName = "Editor";
 
 export default Editor;
