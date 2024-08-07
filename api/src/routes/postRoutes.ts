348b364b489a04dff922d7d9c31d4167c069005b
David <davidfco.pozo@gmail.com>
2024-08-07 19:52:37 +0200

    refactor: Update postRoutes to use getPostBySlugOrId for fetching post by slug or ID
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/routes/postRoutes.ts
+++ api/src/routes/postRoutes.ts
@@ -4,12 +4,11 @@ import { auth } from "../middleware/auth";
 import {
   createPost,
   getAllPosts,
-  getPostById,
   updatePostById,
   deletePostById,
   toggleLike,
   toggleBookmark,
-  getPostBySlug,
+  getPostBySlugOrId,
 } from "../controllers/postController";
 import { visitsCounter } from "../middleware/visits-counter";
 
@@ -17,11 +16,9 @@ router.route("/").get(getAllPosts).post(auth, createPost);
 router.route("/like").put(auth, toggleLike);
 router.route("/bookmark").put(auth, toggleBookmark);
 router
-  .route("/:id")
-  .get(visitsCounter, getPostById)
+  .route("/:slugOrId")
+  .get(visitsCounter, getPostBySlugOrId)
   .put(auth, updatePostById)
   .delete(auth, deletePostById);
-
-router.route("/:slug").get(visitsCounter, getPostBySlug);
 
 export default router;
