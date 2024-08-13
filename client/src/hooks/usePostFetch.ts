d019364507172516aae175abd76eef5c532ddaff
David <davidfco.pozo@gmail.com>
2024-08-13 18:25:47 +0200

    refactor: Update useFetchPost to include isFetching in the returned object
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/hooks/usePostFetch.ts
+++ client/src/hooks/usePostFetch.ts
@@ -1,13 +1,12 @@
-import { useEffect } from "react";
 import useFetchRequest from "./useFetchRequest";
 
 const useFetchPost = (slug: string) => {
-  const { data, error, isLoading } = useFetchRequest(
+  const { data, error, isLoading, isFetching } = useFetchRequest(
     "posts",
     `/api/posts/${slug}`
   );
 
-  return { data, error, isLoading };
+  return { data, error, isLoading, isFetching };
 };
 
 export default useFetchPost;
