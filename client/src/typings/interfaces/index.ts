bdc882f3583f13b2e907a87b5b66c3a418561029
David <davidfco.pozo@gmail.com>
2024-08-13 18:26:52 +0200

    refactor: Update CommentInterface to include replies and likes arrays
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/typings/interfaces/index.ts
+++ client/src/typings/interfaces/index.ts
@@ -1,4 +1,5 @@
 import { FormEvent, HTMLAttributes, ReactNode } from "react";
+import { ObjectId } from "mongoose";
 
 export interface BlogEditorProps {
   onSave: (data: { title: string; content: string }) => void;
@@ -46,4 +47,24 @@ export interface FeatureImageProps {
   imageUrl: string | null;
   temporaryFeatureImage: File | null;
   onUpload: (file: File | null) => void;
+}
+
+export interface CommentInterface {
+  _id: ObjectId;
+  postedBy: ObjectId;
+  post: ObjectId;
+  content: string;
+  replies: Array<{
+    _id: ObjectId;
+    postedBy: ObjectId;
+    post: ObjectId;
+    content: string;
+    replies: any[];
+    likes: ObjectId[];
+    createdAt: Date;
+    updatedAt: Date;
+  }>;
+  likes: ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
 }
