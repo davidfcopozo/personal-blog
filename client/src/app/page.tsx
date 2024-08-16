5571d8a43ad244c5e935b866c85e60f4e86ae110
David <davidfco.pozo@gmail.com>
2024-08-16 19:29:27 +0200

    refactor: Add null check
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   }, [error, toast]);
 
   const blogCards = useMemo(() => {
-    return data?.data.map(
+    return data?.data?.map(
       (post: PostInterface, index: { toString: () => any }) => (
         <BlogCard
           key={post?._id.toString() + index.toString()}
