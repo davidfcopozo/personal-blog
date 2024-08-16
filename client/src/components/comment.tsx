312ccdc737616ea167de22d172d26f6dfb107898
David <davidfco.pozo@gmail.com>
2024-08-16 19:38:05 +0200

    refactor: Render commment and pass replies data to the Reply component
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/comment.tsx
+++ client/src/components/comment.tsx
@@ -5,15 +5,14 @@ import { MessageCircle, ThumbsUp } from "lucide-react";
 import Reply from "./reply";
 import { getRelativeTime } from "@/utils/formats";
 import { CommentProps } from "@/typings/types";
+import useCommentFetch from "@/hooks/useCommentFetch";
 
 const Comment: React.FC<CommentProps> = ({ comment }) => {
-  const { content, _id, replies, createdAt } = comment;
-
-  console.log(content);
+  const { data: fetchedReplies } = useCommentFetch(comment.replies);
 
   return (
     <>
-      <div id={`${_id}`} className="flex items-start gap-4">
+      <div id={`${comment?._id}`} className="flex items-start gap-4">
         <Avatar className="w-10 h-10 border">
           <AvatarImage src="/placeholder-user.jpg" />
           <AvatarFallback>AC</AvatarFallback>
@@ -22,10 +21,10 @@ const Comment: React.FC<CommentProps> = ({ comment }) => {
           <div className="flex items-center gap-2">
             <div className="font-medium">Acme Inc</div>
             <div className="text-xs text-gray-500 dark:text-gray-400">
-              {getRelativeTime(createdAt)}
+              {getRelativeTime(comment?.createdAt!)}
             </div>
           </div>
-          <p>{content}</p>
+          <p>{comment?.content}</p>
           <div className="flex items-center gap-2">
             <Button variant="ghost" size="icon">
               <MessageCircle className="w-4 h-4" />
@@ -37,15 +36,20 @@ const Comment: React.FC<CommentProps> = ({ comment }) => {
               className="flex align-center text-center gap-1"
             >
               <ThumbsUp className="w-4 h-4" />
-              <span className="text-xs text-muted-foreground">5</span>
+              <span className="text-xs text-muted-foreground">
+                {comment.likes.length}
+              </span>
               <span className="sr-only">Like</span>
             </Button>
           </div>
         </div>
       </div>
 
-      {replies &&
-        replies.map((reply) => <Reply key={`${reply._id}`} reply={reply} />)}
+      {fetchedReplies &&
+        fetchedReplies?.length >= 1 &&
+        fetchedReplies.map((reply) => (
+          <Reply key={`${reply?._id}`} reply={reply} />
+        ))}
     </>
   );
 };
