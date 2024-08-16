e666702598e61c3e5d9c77af68d819834f33553f
David <davidfco.pozo@gmail.com>
2024-08-16 19:41:31 +0200

    refactor: Add useCommentFetch hook for fetching comments
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/hooks/useCommentFetch.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { CommentInterface } from "@/typings/interfaces";
+import { useEffect } from "react";
+
+const getBaseURL = () => {
+  if (typeof window !== "undefined") {
+    return `http://${window.location.hostname}:3000`;
+  }
+  return "http://localhost:3000";
+};
+
+const useCommentFetch = (ids: string[]) => {
+  const baseULR = getBaseURL();
+
+  const fetchComments = async (): Promise<CommentInterface[]> => {
+    const commentPromises = ids.map(async (id) => {
+      const { data } = await axios.get<{
+        success: boolean;
+        data: CommentInterface;
+      }>(`${baseULR}/api/comments/${id}`);
+      return data.data;
+    });
+
+    const comments = await Promise.all(commentPromises);
+    return comments;
+  };
+
+  const { data, error, isLoading } = useQuery<CommentInterface[]>({
+    queryKey: ["comments", ...ids],
+    queryFn: fetchComments,
+    refetchOnWindowFocus: false,
+    refetchOnMount: true,
+    gcTime: 0,
+    staleTime: 0,
+  });
+
+  return { data, error, isLoading };
+};
+
+export default useCommentFetch;
