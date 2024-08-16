9814ea8279c108af6ef983c4a958bad3a7feae69
David <davidfco.pozo@gmail.com>
2024-08-16 19:34:40 +0200

    refactor: Update fetch and render comment from server
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/comment-section.tsx
+++ client/src/components/comment-section.tsx
@@ -1,18 +1,19 @@
 import { CommentSectionPropsType } from "@/typings/types";
 import Comment from "./comment";
 import CommentBox from "./comment-box";
+import useCommentFetch from "@/hooks/useCommentFetch";
 
 export default function CommentSection({ comments }: CommentSectionPropsType) {
-  console.log("comments", comments);
+  const { data: fetchedComments } = useCommentFetch(comments);
 
   return (
     <div className="w-full max-w-3xl px-4 mx-auto space-y-6 mb-8 sm:px-6">
       <div className="space-y-4">
         <h2 className="text-2xl font-bold">Comments</h2>
         <div className="grid gap-6">
-          {comments ? (
-            comments.map((comment) => (
-              <Comment key={`${comment._id}`} comment={comment} />
+          {fetchedComments && fetchedComments.length >= 1 ? (
+            fetchedComments?.map((comment) => (
+              <Comment key={`${comment?._id}`} comment={comment} />
             ))
           ) : (
             <p>No comments yet, be the first!</p>
