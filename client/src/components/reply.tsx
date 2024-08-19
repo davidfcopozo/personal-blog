9eb36be0faa5dabe8a0f0053befbf9bc8e4be22b
David <davidfco.pozo@gmail.com>
2024-08-19 18:23:38 +0200

    refactor: Update Replyt component to fetch and display the name and initials of the user who posted the comment
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/reply.tsx
+++ client/src/components/reply.tsx
@@ -2,19 +2,24 @@ import React from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { MessageCircle, ThumbsUp } from "lucide-react";
-import { getRelativeTime } from "@/utils/formats";
+import { getFullName, getNameInitials, getRelativeTime } from "@/utils/formats";
 import { ReplyProps } from "@/typings/types";
+import useFetchRequest from "@/hooks/useFetchRequest";
 
 const Reply: React.FC<ReplyProps> = ({ reply }) => {
+  const { data: postedBy } = useFetchRequest(
+    "commentPostedBy",
+    `/api/users/${reply.postedBy}`
+  );
   return (
     <div key={`${reply?._id}`} className="flex items-start gap-4 pl-14">
       <Avatar className="w-10 h-10 border">
         <AvatarImage src="/placeholder-user.jpg" />
-        <AvatarFallback>JD</AvatarFallback>
+        <AvatarFallback>{getNameInitials(postedBy?.data)}</AvatarFallback>
       </Avatar>
       <div className="grid gap-2 flex-1">
         <div className="flex items-center gap-2">
-          <div className="font-medium">John Doe</div>
+          <div className="font-medium">{getFullName(postedBy?.data)}</div>
           <div className="text-xs text-gray-500 dark:text-gray-400">
             {getRelativeTime(reply?.createdAt!)}
           </div>
