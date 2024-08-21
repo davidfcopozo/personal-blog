57e4744dc967224c9e55670755c98e29b532d014
David <davidfco.pozo@gmail.com>
2024-08-21 19:40:25 +0200

    refactor: Add /:id routes for put and delete requests
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/routes/postRoutes.ts
+++ api/src/routes/postRoutes.ts
@@ -15,10 +15,7 @@ import { visitsCounter } from "../middleware/visits-counter";
 router.route("/").get(getAllPosts).post(auth, createPost);
 router.route("/like").put(auth, toggleLike);
 router.route("/bookmark").put(auth, toggleBookmark);
-router
-  .route("/:slugOrId")
-  .get(visitsCounter, getPostBySlugOrId)
-  .put(auth, updatePostById)
-  .delete(auth, deletePostById);
+router.route("/:slugOrId").get(visitsCounter, getPostBySlugOrId);
+router.route("/:id").put(auth, updatePostById).delete(auth, deletePostById);
 
 export default router;
