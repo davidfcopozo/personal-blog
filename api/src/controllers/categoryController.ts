f6e983b53ac505ea1ceed5ba3efb243d4f4cd160
David <davidfco.pozo@gmail.com>
2024-08-23 19:42:59 +0200

    feat: Update categoryController to handle topic name validation and authorization
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/controllers/categoryController.ts
+++ api/src/controllers/categoryController.ts
@@ -1,7 +1,11 @@
 import Category from "../models/categoryModel";
 import { Response, Request, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
-import { NotFound } from "../errors";
+import { BadRequest, NotFound, Unauthenticated } from "../errors";
+import { categoryValidator, topicNameValidator } from "../utils/validators";
+import Topic from "../models/topicModel";
+import { RequestWithUserInfo } from "../typings/models/user";
+import User from "../models/userModel";
 
 export const createCategory = async (
   req: Request,
@@ -89,15 +93,59 @@ export const getCategoriesByTopic = async (
 };
 
 export const updateCategoryById = async (
-  req: Request,
+  req: RequestWithUserInfo | any,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const { name, description, subcategories } = req.body;
+    const {
+      user: { userId },
+      params: { id: categoryID },
+      body: { name, topic },
+    } = req;
+
+    const cat = await Category.findById(categoryID);
+    const user = await User.findById(userId);
+    const topicName = await Topic.findOne({
+      name: { $regex: new RegExp(topic, "i") },
+    }).lean();
+
+    if (!user) {
+      throw new NotFound("User not found");
+    }
+
+    if (!cat) {
+      throw new NotFound("Post not found");
+    }
+
+    if (!name || !topic) {
+      throw new BadRequest("Please provide a name and topic for the category");
+    }
+
+    if (!categoryValidator(name)) {
+      throw new BadRequest("Invalid input for category name");
+    }
+
+    if (!topicNameValidator(topic)) {
+      throw new BadRequest("Invalid input for topic name");
+    }
+
+    if (!topicName) {
+      throw new NotFound("This topic does not exist");
+    }
+
+    if (
+      (cat && cat?.postedBy?.toString() !== userId) ||
+      user.role !== "admin"
+    ) {
+      throw new Unauthenticated(
+        "You are not authorized to update this category"
+      );
+    }
+
     const category = await Category.findByIdAndUpdate(
-      req.params.id,
-      { name, description, subcategories },
+      { _id: categoryID, postedBy: userId },
+      { name, topic: topicName?._id },
       { new: true, runValidators: true }
     );
     if (!category) {
