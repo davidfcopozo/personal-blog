7dfb3c0be70e80da12f705040f3c6a65cffe5dd1
David <davidfco.pozo@gmail.com>
2024-09-15 15:20:19 +0200

    refactor: Refactor Category page component to improve performance, resource loading  and error handling
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/[...category]/page.tsx
+++ client/src/app/[...category]/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { BlogPostCard } from "@/components/blog-post-card";
+import { PostSkeletonCard } from "@/components/post-skeleton";
 import useFetchRequest from "@/hooks/useFetchRequest";
 import { PostType } from "@/typings/types";
 import { convertSlugToName } from "@/utils/formats";
+import { useEffect, useMemo } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Category({ params }: { params: { category: string } }) {
   const cat = decodeURI(params.category);
+  const { toast } = useToast();
 
   const categoryToDisplay = convertSlugToName(cat);
   const {
@@ -14,18 +18,41 @@ export default function Category({ params }: { params: { category: string } }) {
     isFetching,
   } = useFetchRequest("posts", `/api/posts`);
 
-  const blogPosts: PostType[] = posts?.data.filter((post: PostType) =>
-    post?.categories?.includes(categoryToDisplay)
-  );
+  const blogPosts: PostType[] = useMemo(() => {
+    if (!posts?.data || !Array.isArray(posts.data)) {
+      return [];
+    }
+    return posts?.data?.filter((post: PostType) =>
+      post?.categories?.includes(categoryToDisplay)
+    );
+  }, [posts, cat]);
+
+  useEffect(() => {
+    if (error) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: error.message || "An error occurred",
+      });
+    }
+  }, [error, toast]);
 
   return (
     <div className="container px-4 mt-10 py-8">
       <h1 className="text-3xl font-bold mb-8">{`Latest ${categoryToDisplay} Blog Posts`}</h1>
       <div className="space-y-6 flex justify-center flex-wrap md:justify-normal">
-        {blogPosts &&
+        {isFetching ? (
+          <div className="w-full flex justify-center flex-wrap gap-4 mt-14 sm:w-2/3 md:w-3/4 pt-1 px-2">
+            <PostSkeletonCard />
+            <PostSkeletonCard />
+            <PostSkeletonCard />
+          </div>
+        ) : (
+          blogPosts &&
           blogPosts.map((post) => (
             <BlogPostCard key={post._id.toString()} post={post} />
-          ))}
+          ))
+        )}
       </div>
     </div>
   );
