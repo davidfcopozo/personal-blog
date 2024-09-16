2452bdc7aac9aa7c9925078f0ea708a28981c4a9
David <davidfco.pozo@gmail.com>
2024-09-16 15:55:42 +0200

    refactor: Import PostInterface to fix type issues
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,6 +10,7 @@ import { useToast } from "@/components/ui/use-toast";
 import { CategoryType, PostType } from "@/typings/types";
 import { useRouter } from "next/navigation";
 import CategoriesSkeleton from "@/components/categories-skeleton";
+import { PostInterface } from "../../../api/src/typings/models/post";
 
 export default function Home() {
   const { toast } = useToast();
@@ -76,7 +77,7 @@ export default function Home() {
   const blogCards = useMemo(() => {
     if (Array.isArray(posts?.data))
       return posts?.data?.map(
-        (post: PostType, index: { toString: () => any }) => (
+        (post: PostInterface, index: { toString: () => any }) => (
           <BlogCard
             key={post?._id.toString() + index.toString()}
             post={post}
