ba00494e8eafc5e8e50d10091797dc550fa2133e
David <davidfco.pozo@gmail.com>
2024-09-19 18:44:26 +0200

    Refactor ProfileBlogCard component to improve performance and readability
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/profile-blog-card.tsx
+++ client/src/components/profile-blog-card.tsx
@@ -1,35 +1,26 @@
-import React from "react";
-import { Card, CardContent } from "./ui/card";
 import Link from "next/link";
-import Image from "next/image";
-import { truncateText } from "@/utils/formats";
+import { FC } from "react";
 
 interface BlogPostProps {
   post: any;
 }
 
-const ProfileBlogCard: React.FC<BlogPostProps> = ({ post }) => {
-  const { title, content, featureImg } = post;
+const ProfileBlogCard: FC<BlogPostProps> = ({ post }) => {
+  const { _id, title, visits } = post;
   return (
-    <Card className="group border-0">
-      <Link href="#" className="contents" prefetch={false}>
-        <CardContent className="grid gap-4">
-          <Image
-            src={`${featureImg}`}
-            alt="Article Image"
-            width={400}
-            height={225}
-            className="aspect-video w-full overflow-hidden rounded-lg object-cover transition-all duration-300 group-hover:scale-105"
-          />
-          <div className="grid gap-2">
-            <h4 className="text-lg font-semibold">{title}</h4>
-            <p className="text-muted-foreground line-clamp-3">
-              {truncateText(content, 150)}
-            </p>
-          </div>
-        </CardContent>
+    <div key={_id.toString()} className="border-b pb-4 last:border-b-0">
+      <Link href={`/blog/${_id}`} passHref>
+        <h2 className="text-xl font-semibold hover:text-primary">{title}</h2>
       </Link>
-    </Card>
+      <div className="flex justify-between items-center mt-2 text-sm text-muted-foreground">
+        <span>
+          {post?.createdAt
+            ? new Date(post.createdAt.toString()).toLocaleDateString()
+            : ""}
+        </span>
+        <span>{visits as number} visits</span>
+      </div>
+    </div>
   );
 };
 
