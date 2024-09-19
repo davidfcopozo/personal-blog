a3a7361be032be1d2b3c7308c17aa19fbbbaf5b6
David <davidfco.pozo@gmail.com>
2024-09-19 19:56:22 +0200

    Refactor profile blog post card for a more simple and minimalistic version
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/components/profile-blog-post-card.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { FC } from "react";
+
+interface BlogPostProps {
+  post: any;
+}
+
+const ProfileBlogPostCard: FC<BlogPostProps> = ({ post }) => {
+  const { _id, title, visits } = post;
+  return (
+    <div key={_id.toString()} className="border-b pb-4 last:border-b-0">
+      <Link href={`/blog/${_id}`} passHref>
+        <h2 className="text-xl font-semibold hover:text-primary">{title}</h2>
+      </Link>
+      <div className="flex justify-between items-center mt-2 text-sm text-muted-foreground">
+        <span>
+          {post?.createdAt
+            ? new Date(post.createdAt.toString()).toLocaleDateString()
+            : ""}
+        </span>
+        <span>{visits as number} visits</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileBlogPostCard;
