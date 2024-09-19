445152d9c42aaff49fbfd462248470a18b4eb457
David <davidfco.pozo@gmail.com>
2024-09-19 20:14:55 +0200

    Refactor Profile component to conditionally render a loading skeleton or user profile
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/profile.tsx
+++ client/src/components/profile.tsx
@@ -1,11 +1,20 @@
 "use client";
 import { useAuth } from "@/context/AuthContext";
 import UserProfile from "./user-profile";
+import ProfilePageSkeleton from "./profile-page-skeleton";
 
 const Profile = () => {
-  const { currentUser } = useAuth();
+  const { currentUser, isUserFetching, isUserLoading } = useAuth();
 
-  return <UserProfile user={currentUser?.data} />;
+  return (
+    <div className="min-h-screen mt-14">
+      {isUserFetching || isUserLoading ? (
+        <ProfilePageSkeleton />
+      ) : (
+        <UserProfile user={currentUser?.data} />
+      )}
+    </div>
+  );
 };
 
 export default Profile;
