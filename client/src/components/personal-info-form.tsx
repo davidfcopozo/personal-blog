ba2d6daebab7f9c4f3bc909438c58fd642ea752b
David <davidfco.pozo@gmail.com>
2024-09-24 20:14:09 +0200

    Refactor: Destructured necessary properties out of currentUser object
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/personal-info-form.tsx
+++ client/src/components/personal-info-form.tsx
@@ -5,6 +5,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { UserType } from "@/typings/types";
 
 const PersonalInfoForm = ({ currentUser }: { currentUser: UserType }) => {
+  const { firstName, lastName, email, username, bio } = currentUser;
   return (
     <div className="space-y-4">
       <div className="grid grid-cols-2 gap-4">
@@ -15,45 +16,33 @@ const PersonalInfoForm = ({ currentUser }: { currentUser: UserType }) => {
           <Input
             id="firstName"
             type="text"
-            defaultValue={currentUser?.firstName as string}
+            defaultValue={firstName as string}
           />
         </div>
         <div>
           <Label htmlFor="lastName" className="font-bold">
             Last Name
           </Label>
-          <Input
-            id="lastName"
-            type="text"
-            defaultValue={currentUser?.lastName as string}
-          />
+          <Input id="lastName" type="text" defaultValue={lastName as string} />
         </div>
       </div>
       <div>
         <Label htmlFor="email" className="font-bold">
           Email
         </Label>
-        <Input
-          id="email"
-          type="email"
-          defaultValue={currentUser?.email as string}
-        />
+        <Input id="email" type="email" defaultValue={email as string} />
       </div>
       <div>
         <Label htmlFor="username" className="font-bold">
           Username
         </Label>
-        <Input
-          id="username"
-          type="text"
-          defaultValue={currentUser?.username as string}
-        />
+        <Input id="username" type="text" defaultValue={username as string} />
       </div>
       <div>
         <Label htmlFor="bio" className="font-bold">
           Bio
         </Label>
-        <Textarea id="bio" rows={4} defaultValue={currentUser?.bio as string} />
+        <Textarea id="bio" rows={4} defaultValue={bio as string} />
       </div>
     </div>
   );
