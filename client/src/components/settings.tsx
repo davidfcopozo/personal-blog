34f25a41cd26788ea3bb08a7bf52e3c5e25f0cc5
David <davidfco.pozo@gmail.com>
2024-09-24 19:49:08 +0200

    Refactor: Made component more modular by moving socials form to its own component
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/settings.tsx
+++ client/src/components/settings.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -12,15 +11,12 @@ import { getFullName, getNameInitials } from "@/utils/formats";
 import { useAuth } from "@/context/AuthContext";
 import SkillsForm from "./skills-form";
 import InterestForm from "./interests-form";
+import SocialsForm from "./socials-form";
 
 export const Settings = () => {
   const router = useRouter();
   const { currentUser } = useAuth();
-  const [twitterHandle, setTwitterHandle] = useState("");
-  const [instagramHandle, setInstagramHandle] = useState("");
-  const [githubHandle, setGithubHandle] = useState("");
-  const [linkedinHandle, setLinkedinHandle] = useState("");
-  const [dribbleHandle, setDribbleHandle] = useState("");
+
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -131,80 +127,7 @@ export const Settings = () => {
                     </div>
                   </TabsContent>
                   <TabsContent value="social">
-                    <div className="space-y-4">
-                      <div>
-                        <Label htmlFor="website" className="font-bold">
-                          Website
-                        </Label>
-                        <Input
-                          id="website"
-                          type="text"
-                          defaultValue={currentUser?.data?.website}
-                        />
-                      </div>
-                      <div>
-                        <Label htmlFor="twitter" className="font-bold">
-                          Twitter Handle
-                        </Label>
-                        <div className="flex">
-                          <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                            @
-                          </span>
-                          <Input
-                            id="twitter"
-                            value={twitterHandle}
-                            onChange={(e) => setTwitterHandle(e.target.value)}
-                            className="rounded-l-none"
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <Label htmlFor="instagram" className="font-bold">
-                          Instagram Handle
-                        </Label>
-                        <div className="flex">
-                          <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                            @
-                          </span>
-                          <Input
-                            id="instagram"
-                            value={instagramHandle}
-                            onChange={(e) => setInstagramHandle(e.target.value)}
-                            className="rounded-l-none"
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <Label htmlFor="github" className="font-bold">
-                          GitHub Handle
-                        </Label>
-                        <Input
-                          id="github"
-                          value={githubHandle}
-                          onChange={(e) => setGithubHandle(e.target.value)}
-                        />
-                      </div>
-                      <div>
-                        <Label htmlFor="linkedin" className="font-bold">
-                          LinkedIn Handle
-                        </Label>
-                        <Input
-                          id="linkedin"
-                          value={linkedinHandle}
-                          onChange={(e) => setLinkedinHandle(e.target.value)}
-                        />
-                      </div>
-                      <div>
-                        <Label htmlFor="dribble" className="font-bold">
-                          Dribble Handle
-                        </Label>
-                        <Input
-                          id="dribble"
-                          value={dribbleHandle}
-                          onChange={(e) => setDribbleHandle(e.target.value)}
-                        />
-                      </div>
-                    </div>
+                    <SocialsForm />
                   </TabsContent>
                   <TabsContent value="custom">
                     <div className="space-y-4">
