125e32d8aa100b56681fdd6bae7ccf16b81eb784
David <davidfco.pozo@gmail.com>
2024-10-01 18:33:20 +0200

    Refactor: Update PersonalInfoForm component to destructure currentUser properties and set values for input fields
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/personal-info-form.tsx
+++ client/src/components/personal-info-form.tsx
@@ -2,15 +2,33 @@
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { UserType } from "@/typings/types";
+import { PersonalInfoFormProps } from "@/typings/types";
 
-const PersonalInfoForm = ({ currentUser }: { currentUser: UserType }) => {
+const PersonalInfoForm = ({
+  currentUser,
+  firstName,
+  setFirstName,
+  lastName,
+  setLastName,
+  email,
+  setEmail,
+  username,
+  setUsername,
+  bio,
+  setBio,
+}: PersonalInfoFormProps) => {
   if (!currentUser) {
     return (
       <div className="space-y-4 text-center">Loading user information...</div>
     );
   }
-  const { firstName, lastName, email, username, bio } = currentUser;
+  const {
+    firstName: currentFirstName,
+    lastName: currentLastName,
+    email: currentEmail,
+    username: currentUsername,
+    bio: currentBio,
+  } = currentUser;
   return (
     <div className="space-y-4">
       <div className="grid grid-cols-2 gap-4">
@@ -21,33 +39,54 @@ const PersonalInfoForm = ({ currentUser }: { currentUser: UserType }) => {
           <Input
             id="firstName"
             type="text"
-            defaultValue={firstName as string}
+            value={firstName ? firstName : (currentFirstName as string)}
+            onChange={(e) => setFirstName(e.target.value)}
           />
         </div>
         <div>
           <Label htmlFor="lastName" className="font-bold">
             Last Name
           </Label>
-          <Input id="lastName" type="text" defaultValue={lastName as string} />
+          <Input
+            id="lastName"
+            type="text"
+            value={lastName ? lastName : (currentLastName as string)}
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </div>
       </div>
       <div>
         <Label htmlFor="email" className="font-bold">
           Email
         </Label>
-        <Input id="email" type="email" defaultValue={email as string} />
+        <Input
+          id="email"
+          type="email"
+          value={email ? email : (currentEmail as string)}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
         <Label htmlFor="username" className="font-bold">
           Username
         </Label>
-        <Input id="username" type="text" defaultValue={username as string} />
+        <Input
+          id="username"
+          type="text"
+          value={username ? username : (currentUsername as string)}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div>
         <Label htmlFor="bio" className="font-bold">
           Bio
         </Label>
-        <Textarea id="bio" rows={4} defaultValue={bio as string} />
+        <Textarea
+          id="bio"
+          rows={4}
+          value={bio ? bio : (currentBio as string)}
+          onChange={(e) => setBio(e.target.value)}
+        />
       </div>
     </div>
   );
