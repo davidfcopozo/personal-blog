4555de027f7d49a1da9c0a579d58160066c8303e
David <davidfco.pozo@gmail.com>
2024-10-05 19:05:08 +0200

    Refactor: Add explicit types for SocialMediaProfiles in index.ts
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/typings/types/index.ts
+++ api/src/typings/types/index.ts
@@ -5,3 +5,11 @@ import { UserInterface } from "../models/user";
 export type UserType = UserInterface | null;
 export type PostType = PostInterface | null;
 export type CommentType = CommentInterface | null;
+export type SocialMediaProfiles = {
+  x?: string;
+  linkedIn?: string;
+  github?: string;
+  facebook?: string;
+  instagram?: string;
+  dribble?: string;
+};
