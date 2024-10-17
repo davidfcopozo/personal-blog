977b539bd607a822bdcca3d5823ebee8f716ac37
David <davidfco.pozo@gmail.com>
2024-10-17 19:54:31 +0200

    Refactor: Update CustomBadge component to use unique key prop to avoid name conflict
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/custom-badge.tsx
+++ client/src/components/custom-badge.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { XIcon } from "./icons";
 import { CustomBadgeProps } from "@/typings/interfaces";
 
-const CustomBadge = ({ value, key, onRemove, classes }: CustomBadgeProps) => {
+const CustomBadge = ({
+  value,
+  uniQueKey,
+  onRemove,
+  classes,
+}: CustomBadgeProps) => {
   return (
     <div
-      key={key}
+      key={uniQueKey}
       className="border-transparent cursor-pointer bg-primary text-primary-foreground hover:bg-primary/80 inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
     >
       {value}
