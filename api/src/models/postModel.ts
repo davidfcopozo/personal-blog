ba5ec21c6bf33de373fdfeb205101b29365ccc19
David <davidfco.pozo@gmail.com>
2024-10-18 19:57:04 +0200

    Refactor: Update postModel to use imported mongoose model and schema types
    Corrected categories type
    
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/models/postModel.ts
+++ api/src/models/postModel.ts
@@ -1,13 +1,15 @@
-import mongoose from "mongoose";
+import mongoose, { model } from "mongoose";
 import { PostInterface } from "../typings/models/post";
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
 
-const postSchema = new mongoose.Schema<PostInterface>(
+const postSchema = new Schema<PostInterface>(
   {
     title: { type: String, required: [true, "Title is required"] },
     content: { type: String, required: [true, "Content is required"] },
     slug: { type: String, required: [true, "Slug is required"] },
     postedBy: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: ObjectId,
       ref: "User",
       required: true,
     },
@@ -17,17 +19,17 @@ const postSchema = new mongoose.Schema<PostInterface>(
       default:
         "https://firebasestorage.googleapis.com/v0/b/personal-blog-e0f8c.appspot.com/o/images%2Ffallback-featured-image.webp?alt=media&token=44970380-079b-4d03-80e8-9b322a365e1c",
     },
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    likes: [{ type: ObjectId, ref: "User" }],
+    bookmarks: [{ type: ObjectId, ref: "User" }],
     tags: [{ type: String }],
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+    comments: [{ type: ObjectId, ref: "Comment" }],
     published: { type: Boolean, default: false },
     visits: { type: Number, default: 0 },
-    categories: [{ type: String }],
+    categories: [{ type: ObjectId, ref: "Category" }],
   },
   { timestamps: true }
 );
 
-const Post = mongoose.model("Post", postSchema);
+const Post = model("Post", postSchema);
 
 export default Post;
