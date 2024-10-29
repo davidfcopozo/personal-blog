feb954ff7eddeabf5f179971d59b48754efb45fc
David <davidfco.pozo@gmail.com>
2024-10-29 20:30:07 +0100

    Refactor: Create route to render username/post dynamically
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/api/posts/[slugOrId]/route.ts
+++ client/src/app/api/posts/[slugOrId]/route.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export async function GET(
   req: NextRequest,
@@ -27,6 +28,65 @@ export async function GET(
     return new Response(
       JSON.stringify({
         message: error.response?.data?.msg || "Internal server error",
+      }),
+      {
+        status: error.response?.status || 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { slugOrId: string } }
+) {
+  const { slugOrId } = params;
+
+  const token = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!slugOrId) {
+    return new Response(JSON.stringify({ message: "No slug found" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  if (!token) {
+    return new Response(JSON.stringify({ message: "Unauthorized" }), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  try {
+    const body = await req.json();
+
+    const res = await axios.patch(
+      `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/posts/${slugOrId}`,
+      body,
+      {
+        headers: {
+          Authorization: `Bearer ${token?.accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return new Response(JSON.stringify(res.data), {
+      status: res.status,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: Error | any) {
+    console.error("Update post error:", error);
+
+    return new Response(
+      JSON.stringify({
+        message: error.message || "Internal server error",
+        details: error.response?.data || {},
       }),
       {
         status: error.response?.status || 500,
