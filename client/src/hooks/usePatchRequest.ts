e8d27e96b5be178dc6144197acc31b28c4ae22ca
David <davidfco.pozo@gmail.com>
2024-11-05 20:18:24 +0100

    Refactor: Update usePatchRequest hook to utilize UseMutationRequestProps for improved type safety
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/hooks/usePatchRequest.ts
+++ client/src/hooks/usePatchRequest.ts
@@ -1,30 +1,6 @@
+import { UseMutationRequestProps } from "@/typings/interfaces";
 import { useMutation } from "@tanstack/react-query";
-import axios, { AxiosError } from "axios";
-
-// Generic interface for the context that will be passed between callbacks
-interface MutationContext<TData> {
-  previousData?: TData;
-  newData?: TData;
-  // Add other context properties as needed
-}
-
-// Props interface for the hook
-interface UsePatchRequestProps<TData, TVariables> {
-  url: string;
-  onSuccess?: (data: TData, variables: TVariables) => void;
-  onError?: (
-    error: AxiosError,
-    variables: TVariables,
-    context: MutationContext<TData> | undefined
-  ) => void;
-  onMutate?: (variables: TVariables) => Promise<MutationContext<TData>>;
-  onSettled?: (
-    data: TData | undefined,
-    error: AxiosError | null,
-    variables: TVariables,
-    context: MutationContext<TData> | undefined
-  ) => void;
-}
+import axios from "axios";
 
 const usePatchRequest = <TData = unknown, TVariables = unknown>({
   url,
@@ -32,7 +8,7 @@ const usePatchRequest = <TData = unknown, TVariables = unknown>({
   onError,
   onMutate,
   onSettled,
-}: UsePatchRequestProps<TData, TVariables>) => {
+}: UseMutationRequestProps<TData, TVariables>) => {
   const { mutate, data, status, error } = useMutation({
     mutationFn: async (body: any) => {
       const res = await axios.patch(url, body);
