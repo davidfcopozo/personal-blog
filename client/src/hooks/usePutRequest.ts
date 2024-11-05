9ea603656fd939df07dbe43931a66b36348789b9
David <davidfco.pozo@gmail.com>
2024-11-05 20:13:19 +0100

    Refactor: Implement usePatchRequest hook for better consistency and modularization
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/hooks/usePutRequest.ts
@@ -0,0 +1,25 @@
+import { UseMutationRequestProps } from "@/typings/interfaces";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+const usePatchRequest = <TData = unknown, TVariables = unknown>({
+  url,
+  onSuccess,
+  onError,
+  onMutate,
+  onSettled,
+}: UseMutationRequestProps<TData, TVariables>) => {
+  const { mutate, data, status, error } = useMutation({
+    mutationFn: async (body: any) => {
+      const res = await axios.put(url, body);
+      return res.data.data;
+    },
+    onSuccess: onSuccess,
+    onError: onError,
+    onMutate: onMutate,
+    onSettled: onSettled,
+  });
+  return { mutate, data, status, error };
+};
+
+export default usePatchRequest;
