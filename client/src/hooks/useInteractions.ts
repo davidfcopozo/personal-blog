80212b075b60dc379ba7ed0d54b9f5162e36136e
David <davidfco.pozo@gmail.com>
2024-11-08 19:56:31 +0100

    Refactor: Add useInteractions hook for managing post interactions
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/hooks/useInteractions.ts
@@ -0,0 +1,93 @@
+import { useQueryClient } from "@tanstack/react-query";
+import usePutRequest from "./usePutRequest";
+import { useToast } from "@/components/ui/use-toast";
+import { PostFetchType } from "@/typings/types";
+import { ObjectId } from "mongoose";
+import { PostInterface } from "../../../api/src/typings/models/post";
+
+export const useInteractions = () => {
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+
+  const likeMutation = usePutRequest({
+    url: "/api/posts/like",
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["posts"], exact: true });
+
+      toast({
+        title: "Success",
+        description: "Your action was successful.",
+      });
+    },
+    onError: (error) => {
+      const previousPosts = queryClient.getQueryData<PostFetchType>(["posts"]);
+      queryClient.setQueryData(["posts"], previousPosts);
+
+      const errorMessage =
+        error && typeof error === "object"
+          ? `Failed to process the request. ${JSON.stringify(error, null, 2)}`
+          : "Failed to process the request. Please try again.";
+
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: errorMessage,
+      });
+    },
+    onMutate: async (postId: object) => {
+      await queryClient.cancelQueries({ queryKey: ["posts"], exact: true });
+
+      const previousPostsData = queryClient.getQueryData<PostFetchType>([
+        "posts",
+      ]);
+      const previousPosts = previousPostsData?.data;
+      if (!Array.isArray(previousPosts)) {
+        console.error("Expected posts to be an array, but got:", previousPosts);
+        return { previousData: previousPosts };
+      }
+
+      queryClient.setQueryData(["posts"], (oldPosts: PostFetchType) => {
+        if (!Array.isArray(oldPosts?.data)) {
+          console.error(
+            "Expected oldPosts to be an array during update:",
+            oldPosts?.data
+          );
+          return oldPosts;
+        }
+
+        return {
+          ...oldPosts,
+          data: oldPosts.data.map((post: PostInterface) => {
+            if (post._id.toString() === postId.toString()) {
+              const userId = queryClient.getQueryData<{
+                data: { _id: ObjectId };
+              }>(["currentUser"])?.data._id;
+              const userIdString = userId?.toString();
+              const isLiked = post.likes?.some(
+                (like) => like.toString() === userIdString
+              );
+
+              return {
+                ...post,
+                likes: isLiked
+                  ? post.likes?.filter(
+                      (like) => like.toString() !== userIdString
+                    )
+                  : [...(post.likes || []), userId!],
+              };
+            }
+            return post;
+          }),
+        };
+      });
+
+      return { previousData: previousPosts };
+    },
+  });
+
+  const likeInteraction = (postId: string, url: string) => {
+    likeMutation.mutate({ postId });
+  };
+
+  return { likeInteraction  };
+};
