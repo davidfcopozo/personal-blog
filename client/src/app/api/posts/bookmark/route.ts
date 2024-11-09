ef6aa8dcc9e53600fcab4cb50eee99e315c5c3aa
David <davidfco.pozo@gmail.com>
2024-11-09 20:49:51 +0100

    Refactor: Add bookmark API route with token validation and error handling
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/app/api/posts/bookmark/route.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { getToken } from "next-auth/jwt";
+import { NextRequest } from "next/server";
+
+export async function PUT(req: NextRequest) {
+  const token = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    return new Response(JSON.stringify({ message: "No token found" }), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  try {
+    const body = await req.json();
+    const { postId } = body;
+    const res = await axios.put(
+      `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/posts/bookmark`,
+      { postId },
+      {
+        headers: {
+          Authorization: `Bearer ${token.accessToken}`,
+        },
+      }
+    );
+
+    return new Response(JSON.stringify(res.data), {
+      status: res.status,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: Error | any) {
+    return new Response(
+      JSON.stringify({
+        message: error.response?.data?.msg || "Internal server error",
+      }),
+      {
+        status: error.response?.status || 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
