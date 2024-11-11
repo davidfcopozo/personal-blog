81779a16af2ced4685236e5c84dcc995e62effc2
David <davidfco.pozo@gmail.com>
2024-11-11 20:56:18 +0100

    Refactor: Add POST endpoint for comments with token validation and error handling
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/api/comments/[id]/route.ts
+++ client/src/app/api/comments/[id]/route.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getToken } from "next-auth/jwt";
 import { NextRequest } from "next/server";
 
 export async function GET(
@@ -17,6 +18,54 @@ export async function GET(
   try {
     const res = await axios.get(
       `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/comments/${id}`
+    );
+
+    return new Response(JSON.stringify(res.data), {
+      status: res.status,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: Error | any) {
+    return new Response(
+      JSON.stringify({
+        message: error.response?.data?.msg || "Internal server error",
+      }),
+      {
+        status: error.response?.status || 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const token = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    return new Response(JSON.stringify({ message: "No token found" }), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  try {
+    const body = await req.json();
+    const { content } = body;
+
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/comments/${id}`,
+      { content },
+      {
+        headers: {
+          Authorization: `Bearer ${token.accessToken}`,
+        },
+      }
     );
 
     return new Response(JSON.stringify(res.data), {
