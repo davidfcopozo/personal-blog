6469611ce9f786c9c68f4fd4b178ccc7c1bc311b
David <davidfco.pozo@gmail.com>
2024-11-26 21:41:40 +0100

    Refactor: Replace useCommentFetch with useBulkFetch for improved data handling and update comment rendering to use NestedComment component
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/comment-section.tsx
+++ client/src/components/comment-section.tsx
@@ -1,18 +1,22 @@
 import { CommentSectionPropsType } from "@/typings/types";
-import Comment from "./comment";
 import CommentBox from "./comment-box";
-import useCommentFetch from "@/hooks/useCommentFetch";
+import useBulkFetch from "@/hooks/useBulkFetch";
 import { CommentInterface } from "@/typings/interfaces";
+import NestedComment from "./NestedComments";
 
 export default function CommentSection({
   comments,
   id,
   post,
 }: CommentSectionPropsType) {
-  const { data: fetchedComments } = useCommentFetch(comments, "comments");
+  const { data: fetchedComments } = useBulkFetch({
+    ids: comments?.length >= 1 ? comments : [],
+    key: "comments",
+    url: `/api/comments`,
+  });
 
   return (
-    <div className="w-full max-w-3xl px-4 mx-auto space-y-6 mb-8 sm:px-6">
+    <section className="w-full max-w-3xl px-4 mx-auto space-y-6 mb-8 sm:px-6">
       <div className="space-y-4">
         <h2 id="comments-section" className="text-2xl font-bold">
           Comments
@@ -23,13 +27,17 @@ export default function CommentSection({
             fetchedComments
               .filter((comment: CommentInterface) => !comment.isReply)
               .map((comment: CommentInterface) => (
-                <Comment key={`${comment._id}`} comment={comment} post={post} />
+                <NestedComment
+                  key={`${comment._id}`}
+                  comment={comment}
+                  post={post}
+                />
               ))
           ) : (
             <p>No comments yet, be the first!</p>
           )}
         </div>
       </div>
-    </div>
+    </section>
   );
 }
