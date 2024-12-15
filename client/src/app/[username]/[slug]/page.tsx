7c56d2e2c04c4aea167d1ef06e9253c9b3c9cc59
David <davidfco.pozo@gmail.com>
2024-12-15 15:50:33 +0100

    feat: Integrate post fetching and interaction handling in Blog component
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/[username]/[slug]/page.tsx
+++ client/src/app/[username]/[slug]/page.tsx
@@ -1,11 +1,46 @@
+"use client";
 import BlogPost from "@/components/blog-post";
+import SinglePostSkeleton from "@/components/single-post-skeleton";
+import useFetchPost from "@/hooks/useFetchPost";
+import { useInteractions } from "@/hooks/useInteractions";
+import { useEffect, useState } from "react";
 
-const Blog = async ({ params }: { params: { slug: string } }) => {
+const Blog = ({ params }: { params: { slug: string } }) => {
   const slug = decodeURI(params.slug);
+  const { data, isFetching, isLoading } = useFetchPost(slug);
+  const [hasInitialData, setHasInitialData] = useState(false);
+
+  const {
+    handleLikeClick,
+    handleBookmarkClick,
+    liked,
+    bookmarked,
+    amountOfBookmarks,
+    amountOfLikes,
+  } = useInteractions(data?.data);
+
+  useEffect(() => {
+    if (data?.data && !hasInitialData) {
+      setHasInitialData(true);
+    }
+  }, [data?.data, hasInitialData]);
+
+  if (isLoading || (isFetching && !hasInitialData)) {
+    return <SinglePostSkeleton />;
+  }
 
   return (
     <div>
-      <BlogPost slug={slug} />
+      <BlogPost
+        slug={slug}
+        handleLikeClick={handleLikeClick}
+        handleBookmarkClick={handleBookmarkClick}
+        liked={liked}
+        bookmarked={bookmarked}
+        amountOfBookmarks={amountOfBookmarks}
+        amountOfLikes={amountOfLikes}
+        post={data?.data}
+      />
     </div>
   );
 };
