6364fe51785f5de1b860f7f11b4225fa54e16e0a
David <davidfco.pozo@gmail.com>
2024-12-19 19:45:15 +0100

    refactor: Remove unused useState import in CommentEditor component
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/comment-editor.tsx
+++ client/src/components/comment-editor.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import ReactQuill from "react-quill";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
