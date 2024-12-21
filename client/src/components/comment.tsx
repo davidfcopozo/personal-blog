0fe0c5f7ffe7aa64f7aa75f4b0e20af24bd4d488
David <davidfco.pozo@gmail.com>
2024-12-21 12:01:58 +0100

    fix: Remove unused like button implementation in Comment component
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/comment.tsx
+++ client/src/components/comment.tsx
@@ -169,48 +169,6 @@ const Comment: React.FC<CommentProps> = ({ comment, post }) => {
                   isActivated={commentLiked}
                   horizontalCount
                 />
-                {/* <Button
-                  variant="ghost"
-                  size="icon"
-                  type="button"
-                  onClick={handleLikeClick}
-                  className="group flex items-center focus:outline-none transition-colors duration-300 hover:bg-transparent hover:shadow-[inset_0px_0px_40px_0px_rgba(73,134,255,0.2)] "
-                >
-                  <div className="relative">
-                    <ThumbsUp
-                      className={`h-4 w-4 transition-colors duration-300 ${
-                        commentLiked ? "stroke-[#49a4ff]" : "stroke-white"
-                      }`}
-                    />
-                    <ThumbsUp
-                      className={`absolute inset-0 h-4 w-4 text-[#49a4ff] transition-all duration-300 ${
-                        commentLiked
-                          ? "scale-100 opacity-100"
-                          : "scale-0 opacity-0"
-                      }`}
-                    />
-                  </div>
-                  <div className="relative w-4 h-4 overflow-hidden">
-                    <div
-                      className={`absolute inset-0 flex items-center justify-center transition-all duration-300 ${
-                        commentLiked ? "-translate-y-full" : "translate-y-0"
-                      }`}
-                    >
-                      <span className="text-sm text-center">
-                        {commentLikesCount}
-                      </span>
-                    </div>
-                    <div
-                      className={`absolute inset-0 flex items-center justify-center transition-all duration-300 ${
-                        commentLiked ? "translate-y-0" : "translate-y-full"
-                      }`}
-                    >
-                      <span className="text-sm text-center text-[#49a4ff]">
-                        {commentLikesCount}
-                      </span>
-                    </div>
-                  </div>
-                </Button> */}
               </div>
             </div>
           </div>
