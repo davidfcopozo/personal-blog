d49cf50f3232da44cfbc49d7f2b15007584fba94
David <davidfco.pozo@gmail.com>
2024-12-22 15:01:37 +0100

    feat: Implement DELETE endpoint for posts with error handling and authorization
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/api/posts/[slugOrId]/route.ts
+++ client/src/app/api/posts/[slugOrId]/route.ts
@@ -95,3 +95,58 @@ export async function PATCH(
     );
   }
 }
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { slugOrId: string } }
+) {
+  const { slugOrId } = params;
+
+  const token = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!slugOrId) {
+    return new Response(JSON.stringify({ message: "No slug found" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  if (!token) {
+    return new Response(JSON.stringify({ message: "Unauthorized" }), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  try {
+    const res = await axios.delete(
+      `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/posts/${slugOrId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token?.accessToken}`,
+        },
+      }
+    );
+
+    return new Response(JSON.stringify(res.data), {
+      status: res.status,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: Error | any) {
+    console.error("Delete post error:", error);
+
+    return new Response(
+      JSON.stringify({
+        message: error.message || "Internal server error",
+        details: error.response?.data || {},
+      }),
+      {
+        status: error.response?.status || 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
