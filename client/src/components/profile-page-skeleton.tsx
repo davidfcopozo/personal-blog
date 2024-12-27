a325dbce77ae69f5e7a7617308e9083b4865eb80
David <davidfco.pozo@gmail.com>
2024-12-27 16:44:31 +0100

    feat: Restructure skeleton loading state to match profile page layout
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/profile-page-skeleton.tsx
+++ client/src/components/profile-page-skeleton.tsx
@@ -4,8 +4,30 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 const ProfilePageSkeleton = () => {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-8 py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          {/* Blog Posts Skeleton */}
+          <div className="md:col-span-2">
+            <Card>
+              <CardHeader>
+                <Skeleton className="h-8 w-[200px]" />
+              </CardHeader>
+              <CardContent>
+                <div className="space-y-4">
+                  {[1, 2, 3, 4].map((i) => (
+                    <div key={i} className="border-b pb-4 last:border-b-0">
+                      <Skeleton className="h-6 w-full mb-2" />
+                      <div className="flex justify-between items-center mt-2">
+                        <Skeleton className="h-4 w-[100px]" />
+                        <Skeleton className="h-4 w-[80px]" />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+
           {/* Profile Information Skeleton */}
           <div className="md:col-span-1">
             <Card>
@@ -26,28 +48,6 @@ const ProfilePageSkeleton = () => {
                     ))}
                   </div>
                   <Skeleton className="h-10 w-full" />
-                </div>
-              </CardContent>
-            </Card>
-          </div>
-
-          {/* Blog Posts Skeleton */}
-          <div className="md:col-span-2">
-            <Card>
-              <CardHeader>
-                <Skeleton className="h-8 w-[200px]" />
-              </CardHeader>
-              <CardContent>
-                <div className="space-y-4">
-                  {[1, 2, 3, 4].map((i) => (
-                    <div key={i} className="border-b pb-4 last:border-b-0">
-                      <Skeleton className="h-6 w-full mb-2" />
-                      <div className="flex justify-between items-center mt-2">
-                        <Skeleton className="h-4 w-[100px]" />
-                        <Skeleton className="h-4 w-[80px]" />
-                      </div>
-                    </div>
-                  ))}
                 </div>
               </CardContent>
             </Card>
