b60772b59496adba38f49b14af4d9b72aa189574
David <davidfco.pozo@gmail.com>
2025-01-11 11:59:38 +0100

    feat: Add SkillsInterestsManager component for managing user skills and interests instead of two individual components
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/components/SkillsInterestsManager.tsx
@@ -0,0 +1,224 @@
+"use client";
+import React, { FormEvent, KeyboardEvent, MouseEvent } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Badge } from "@/components/ui/badge";
+import { X } from "lucide-react";
+import useFetchRequest from "@/hooks/useFetchRequest";
+import {
+  SingleInterestType,
+  SingleSkillType,
+  SkillsInterestsProps,
+} from "@/typings/types";
+
+const SkillsInterestsManager = <
+  T extends SingleSkillType | SingleInterestType
+>({
+  items,
+  setItems,
+  label,
+  placeholder,
+  fetchUrl,
+}: SkillsInterestsProps<T>) => {
+  const { data: categoriesOrTopicsOrTopics } = useFetchRequest(
+    [fetchUrl],
+    `/api/${fetchUrl}`
+  );
+  const [availableItems, setAvailableItems] = useState<T[]>([]);
+  const [itemsSearchQuery, setItemsSearchQuery] = useState("");
+  const [isItemsInputFocused, setIsItemsInputFocused] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(-1);
+  const [newItem, setNewItem] = useState<T | null>(null);
+  const resultsRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleAddItem = (e: FormEvent) => {
+    e.preventDefault();
+    if (
+      newItem &&
+      items &&
+      !items.some((item) => item._id.toString() === newItem._id.toString())
+    ) {
+      setItems([...items, newItem as unknown as T]);
+      setAvailableItems((prevAvailableItems) =>
+        prevAvailableItems.filter(
+          (availableItem) => availableItem._id !== (newItem as T)._id
+        )
+      );
+      setNewItem(null);
+    }
+  };
+
+  const handleMouseDown = (e: MouseEvent) => {
+    e.preventDefault();
+    // Refocus the input field when clicking inside the results to avoid window blur
+    inputRef.current?.focus();
+  };
+
+  const selectItem = (item: T) => {
+    setItems([...(items || []), item]);
+    setAvailableItems((prevAvailableItems) =>
+      prevAvailableItems.filter(
+        (availableItem) => availableItem._id.toString() !== item._id.toString()
+      )
+    );
+    setItemsSearchQuery("");
+    setHighlightedIndex(-1);
+  };
+
+  const handleRemoveItem = (itemToRemove: string) => {
+    setItems(
+      (items || []).filter((item: T) => item._id.toString() !== itemToRemove)
+    );
+    setAvailableItems([
+      ...(availableItems.filter(
+        (item): item is T => item !== undefined
+      ) as T[]),
+      (items || []).find((item) => item._id.toString() === itemToRemove) as T,
+    ]);
+  };
+
+  const handleClickOutside = (e: globalThis.MouseEvent) => {
+    if (
+      resultsRef.current &&
+      !resultsRef.current.contains(e.target as Node) &&
+      inputRef.current &&
+      !inputRef.current.contains(e.target as Node)
+    ) {
+      setIsItemsInputFocused(false);
+      setHighlightedIndex(-1);
+    }
+  };
+
+  useEffect(() => {
+    if (isItemsInputFocused) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isItemsInputFocused]);
+
+  const filteredItems = useMemo(() => {
+    if (
+      !categoriesOrTopicsOrTopics?.data ||
+      !Array.isArray(categoriesOrTopicsOrTopics.data)
+    ) {
+      return [];
+    }
+    return categoriesOrTopicsOrTopics.data.filter((item: T) =>
+      item.name.toLowerCase().includes(itemsSearchQuery.toLowerCase())
+    );
+  }, [categoriesOrTopicsOrTopics, itemsSearchQuery]);
+
+  useEffect(() => {
+    setAvailableItems(filteredItems);
+  }, [filteredItems]);
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "ArrowDown") {
+      if (highlightedIndex === availableItems.length - 1) {
+        setHighlightedIndex(-1);
+      }
+      setHighlightedIndex((prevIndex) =>
+        Math.min(prevIndex + 1, availableItems.length - 1)
+      );
+    } else if (e.key === "ArrowUp") {
+      if (highlightedIndex === -1 || highlightedIndex === 0) {
+        setHighlightedIndex(availableItems.length);
+      }
+      setHighlightedIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+    } else if (e.key === "Enter" && highlightedIndex >= 0) {
+      e.preventDefault();
+      selectItem(availableItems[highlightedIndex]);
+    }
+  };
+
+  useEffect(() => {
+    // Auto-scroll the results when navigating with the keyboard
+    if (
+      resultsRef.current &&
+      highlightedIndex >= 0 &&
+      resultsRef.current.children[highlightedIndex]
+    ) {
+      const highlightedOption = resultsRef.current.children[
+        highlightedIndex
+      ] as HTMLElement;
+      highlightedOption.scrollIntoView({
+        block: "nearest",
+      });
+    }
+  }, [highlightedIndex]);
+
+  return (
+    <div className="space-y-4">
+      <div>
+        <Label className="font-bold text-md ">{label}</Label>
+        <div className="flex flex-wrap gap-2 my-2">
+          {Array.isArray(items) &&
+            items?.map((item: T) => (
+              <Badge
+                key={`${item._id}`}
+                variant="secondary"
+                className="text-sm px-4 py-2"
+                aria-label={`${item.name}`}
+              >
+                {item.name}
+                <button
+                  type="button"
+                  onClick={() => handleRemoveItem(item._id.toString())}
+                  className="ml-2 text-gray-500 hover:text-gray-700"
+                >
+                  <X className="h-3 w-3" />
+                </button>
+              </Badge>
+            ))}
+        </div>
+        <div className="flex gap-2">
+          <div className="relative w-full">
+            <Input
+              ref={inputRef}
+              value={itemsSearchQuery}
+              onChange={(e) => setItemsSearchQuery(e.target.value)}
+              onFocus={() => setIsItemsInputFocused(true)}
+              onKeyDown={handleKeyDown}
+              placeholder={placeholder}
+            />
+            {availableItems?.length > 0 &&
+              isItemsInputFocused &&
+              itemsSearchQuery && (
+                <div
+                  ref={resultsRef}
+                  onMouseDown={handleMouseDown}
+                  id="results"
+                  className="absolute z-30 max-h-40 overflow-y-scroll [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-muted-foreground [&::-webkit-scrollbar-thumb]:bg-background [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-thumb]:rounded-full mt-2 bg-background rounded-md shadow-sm"
+                >
+                  {availableItems?.map((item: T, index: number) => (
+                    <div
+                      key={`${item._id}`}
+                      onClick={() => selectItem(item)}
+                      className={`px-4 py-2 cursor-pointer hover:bg-muted-foreground hover:text-background ${
+                        highlightedIndex === index &&
+                        "bg-foreground text-background"
+                      }`}
+                    >
+                      {item.name}
+                    </div>
+                  ))}
+                </div>
+              )}
+          </div>
+          <Button type="submit" variant="outline" onClick={handleAddItem}>
+            Add
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SkillsInterestsManager;
--- client/src/components/settings.tsx
+++ client/src/components/settings.tsx
@@ -4,57 +4,37 @@ import { Button } from "@/components/ui/button";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getFullName, getNameInitials } from "@/utils/formats";
-import { useAuth } from "@/context/AuthContext";
+/* import { useAuth } from "@/context/AuthContext"; */
 import SkillsForm from "./skills-form";
 import InterestForm from "./interests-form";
 import SocialsForm from "./socials-form";
 import PersonalInfoForm from "./personal-info-form";
 import { useUpdateSettings } from "@/hooks/useUpdateSettings";
+import SkillsInterestsManager from "./SkillsInterestsManager";
 
 export const Settings = () => {
-  const { currentUser } = useAuth();
+  /*  const { currentUser } = useAuth(); */
 
   const {
+    formData,
+    handleFieldChange,
+    handleSocialMediaChange,
     handleSubmit,
-    firstName,
-    setFirstName,
-    lastName,
-    setLastName,
-    email,
-    setEmail,
-    username,
-    setUsername,
-    bio,
-    setBio,
-    website,
-    setWebsite,
-    twitterHandle,
-    setTwitterHandle,
-    instagramHandle,
-    setInstagramHandle,
-    githubHandle,
-    setGithubHandle,
-    linkedinHandle,
-    setLinkedinHandle,
-    dribbleHandle,
-    setDribbleHandle,
-    skills,
-    setSkills,
-    interests,
-    setInterests,
+    userData,
   } = useUpdateSettings();
 
   return (
     <div className="flex flex-col mt-16 md:mt-12 min-h-[100dvh]">
       <div className="container mx-auto px-6 sm:py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-2 sm:gap-6">
+          {/* User Info Section */}
           <div className="col-span-1 md:col-span-1">
             <div className="bg-background shadow-md rounded-lg p-6">
               <div className="flex flex-col items-center justify-center mb-4">
                 <Avatar className="h-48 w-48 mb-4">
                   <AvatarImage src="/placeholder-user.jpg" />
                   <AvatarFallback className="text-lg font-bold">
-                    {getNameInitials(currentUser)}
+                    {getNameInitials(userData)}
                   </AvatarFallback>
                 </Avatar>
                 <div className="flex flex-col space-y-2 ">
@@ -66,18 +46,17 @@ export const Settings = () => {
               </div>
               <div className="text-center">
                 <h2 className="text-xl font-bold mb-2">
-                  {getFullName(currentUser)}
+                  {getFullName(userData)}
                 </h2>
-                <p className="text-gray-500 mb-2">
-                  @{currentUser?.data?.username}
-                </p>
-                <p className="text-gray-500 mb-2">{currentUser?.data?.email}</p>
+                <p className="text-gray-500 mb-2">@{userData?.username}</p>
+                <p className="text-gray-500 mb-2">{userData?.email}</p>
                 <Link href="" className="text-gray-500 mb-2">
-                  {currentUser?.data?.website}
+                  {userData?.website}
                 </Link>
               </div>
             </div>
           </div>
+          {/* Tabs Section */}
           <div className="col-span-2 md:col-span-2">
             <div className="bg-background shadow-md rounded-lg p-6">
               <Tabs defaultValue="personal" className="w-full">
@@ -86,65 +65,85 @@ export const Settings = () => {
                   <TabsTrigger value="social">Social Media</TabsTrigger>
                   <TabsTrigger value="custom">Custom Experience</TabsTrigger>
                 </TabsList>
-                <form
-                  onSubmit={(e) =>
-                    handleSubmit(e, {
-                      firstName,
-                      lastName,
-                      email,
-                      username,
-                      bio,
-                      socialMediaProfiles: {
-                        x: twitterHandle,
-                        instagram: instagramHandle,
-                        github: githubHandle,
-                        linkedIn: linkedinHandle,
-                        dribble: dribbleHandle,
-                      },
-                      skills,
-                      interests,
-                      website,
-                    })
-                  }
-                >
+                <form onSubmit={handleSubmit}>
+                  {/* Personal Info Tab */}
                   <TabsContent value="personal">
                     <PersonalInfoForm
-                      currentUser={currentUser?.data}
-                      firstName={firstName}
-                      setFirstName={setFirstName}
-                      lastName={lastName}
-                      setLastName={setLastName}
-                      email={email}
-                      setEmail={setEmail}
-                      username={username as string}
-                      setUsername={setUsername}
-                      bio={bio as string}
-                      setBio={setBio}
+                      currentUser={userData}
+                      firstName={formData.firstName as string}
+                      setFirstName={(value) =>
+                        handleFieldChange("firstName", value)
+                      }
+                      lastName={formData.lastName as string}
+                      setLastName={(value) =>
+                        handleFieldChange("lastName", value)
+                      }
+                      email={formData.email as string}
+                      setEmail={(value) => handleFieldChange("email", value)}
+                      username={formData.username as string}
+                      setUsername={(value) =>
+                        handleFieldChange("username", value)
+                      }
+                      bio={formData.bio as string}
+                      setBio={(value) => handleFieldChange("bio", value)}
                     />
                   </TabsContent>
+                  {/* Social Media Tab */}
                   <TabsContent value="social">
                     <SocialsForm
-                      currentUser={currentUser?.data}
-                      website={website}
-                      setWebsite={setWebsite}
-                      twitterHandle={twitterHandle}
-                      setTwitterHandle={setTwitterHandle}
-                      instagramHandle={instagramHandle}
-                      setInstagramHandle={setInstagramHandle}
-                      githubHandle={githubHandle}
-                      setGithubHandle={setGithubHandle}
-                      linkedinHandle={linkedinHandle}
-                      setLinkedinHandle={setLinkedinHandle}
-                      dribbleHandle={dribbleHandle}
-                      setDribbleHandle={setDribbleHandle}
+                      currentUser={userData}
+                      website={formData.website as string}
+                      setWebsite={(value) =>
+                        handleFieldChange("website", value)
+                      }
+                      twitterHandle={formData.socialMediaProfiles?.x as string}
+                      setTwitterHandle={(value) =>
+                        handleSocialMediaChange("x", value)
+                      }
+                      instagramHandle={
+                        formData.socialMediaProfiles?.instagram as string
+                      }
+                      setInstagramHandle={(value) =>
+                        handleSocialMediaChange("instagram", value)
+                      }
+                      githubHandle={
+                        formData.socialMediaProfiles?.github as string
+                      }
+                      setGithubHandle={(value) =>
+                        handleSocialMediaChange("github", value)
+                      }
+                      linkedinHandle={
+                        formData.socialMediaProfiles?.linkedIn as string
+                      }
+                      setLinkedinHandle={(value) =>
+                        handleSocialMediaChange("linkedIn", value)
+                      }
+                      dribbleHandle={
+                        formData.socialMediaProfiles?.dribble as string
+                      }
+                      setDribbleHandle={(value) =>
+                        handleSocialMediaChange("dribble", value)
+                      }
                     />
                   </TabsContent>
+                  {/* Custom Experience Tab */}
                   <TabsContent value="custom">
                     <div className="space-y-4">
-                      <SkillsForm setSkills={setSkills} skills={skills} />
-                      <InterestForm
-                        setInterests={setInterests}
-                        interests={interests}
+                      <SkillsInterestsManager
+                        items={formData.skills}
+                        setItems={(value) => handleFieldChange("skills", value)}
+                        label="Skills"
+                        placeholder="Search for skills..."
+                        fetchUrl="categories"
+                      />
+                      <SkillsInterestsManager
+                        items={formData.interests}
+                        setItems={(value) =>
+                          handleFieldChange("interests", value)
+                        }
+                        label="Interests"
+                        placeholder="Search for interests..."
+                        fetchUrl="topics"
                       />
                     </div>
                   </TabsContent>
