6bbf855f8091dc5810b0b79d3e1b3d7fa6d75557
David <davidfco.pozo@gmail.com>
2025-01-11 16:17:29 +0100

    refactor: Enhance Settings component layout with improved user info display and loading state handling
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/settings.tsx
+++ client/src/components/settings.tsx
@@ -8,6 +8,8 @@ import SocialsForm from "./socials-form";
 import PersonalInfoForm from "./personal-info-form";
 import { useUpdateSettings } from "@/hooks/useUpdateSettings";
 import SkillsInterestsManager from "./SkillsInterestsManager";
+import { SettingsSkeleton } from "./settings-skeleton";
+import { Card } from "./ui/card";
 
 export const Settings = () => {
   const {
@@ -16,45 +18,18 @@ export const Settings = () => {
     handleSocialMediaChange,
     handleSubmit,
     userData,
-    status,
+    isUserPending,
   } = useUpdateSettings();
+
+  if (isUserPending) return <SettingsSkeleton />;
 
   return (
     <div className="flex flex-col mt-16 md:mt-12 min-h-[100dvh]">
       <div className="container mx-auto px-6 sm:py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-2 sm:gap-6">
-          {/* User Info Section */}
-          <div className="col-span-1 md:col-span-1">
-            <div className="bg-background shadow-md rounded-lg p-6">
-              <div className="flex flex-col items-center justify-center mb-4">
-                <Avatar className="h-48 w-48 mb-4">
-                  <AvatarImage src="/placeholder-user.jpg" />
-                  <AvatarFallback className="text-lg font-bold">
-                    {getNameInitials(userData)}
-                  </AvatarFallback>
-                </Avatar>
-                <div className="flex flex-col space-y-2 ">
-                  <Button variant="default">Change picture</Button>
-                  <Button type="button" variant="outline">
-                    Delete picture
-                  </Button>
-                </div>
-              </div>
-              <div className="text-center">
-                <h2 className="text-xl font-bold mb-2">
-                  {getFullName(userData)}
-                </h2>
-                <p className="text-gray-500 mb-2">@{userData?.username}</p>
-                <p className="text-gray-500 mb-2">{userData?.email}</p>
-                <Link href="" className="text-gray-500 mb-2">
-                  {userData?.website}
-                </Link>
-              </div>
-            </div>
-          </div>
           {/* Tabs Section */}
           <div className="col-span-2 md:col-span-2">
-            <div className="bg-background shadow-md rounded-lg p-6">
+            <Card className="bg-background shadow-md rounded-lg p-6">
               <Tabs defaultValue="personal" className="w-full">
                 <TabsList className="grid w-full grid-cols-3">
                   <TabsTrigger value="personal">Personal Info</TabsTrigger>
@@ -67,7 +42,7 @@ export const Settings = () => {
                     <PersonalInfoForm
                       formData={formData}
                       handleFieldChange={handleFieldChange}
-                      isPending={status === "pending"}
+                      isPending={isUserPending}
                     />
                   </TabsContent>
                   {/* Social Media Tab */}
@@ -104,7 +79,36 @@ export const Settings = () => {
                   </div>
                 </form>
               </Tabs>
-            </div>
+            </Card>
+          </div>
+          {/* User Info Section */}
+          <div className="col-span-1 md:col-span-1">
+            <Card className="bg-background shadow-md rounded-lg p-4">
+              <div className="flex flex-col items-center justify-center mb-4">
+                <Avatar className="h-48 w-48 mb-4">
+                  <AvatarImage src="/placeholder-user.jpg" />
+                  <AvatarFallback className="text-lg font-bold">
+                    {getNameInitials(userData)}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col space-y-2 ">
+                  <Button variant="default">Change picture</Button>
+                  <Button type="button" variant="outline">
+                    Delete picture
+                  </Button>
+                </div>
+              </div>
+              <div className="text-center">
+                <h2 className="text-xl font-bold mb-2">
+                  {getFullName(userData)}
+                </h2>
+                <p className="text-gray-500 mb-2">@{userData?.username}</p>
+                <p className="text-gray-500 mb-2">{userData?.email}</p>
+                <Link href="" className="text-gray-500 mb-2">
+                  {userData?.website}
+                </Link>
+              </div>
+            </Card>
           </div>
         </div>
       </div>
