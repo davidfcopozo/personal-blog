2013a5ca15d8ddc096c47de497587b37c620119e
David <davidfco.pozo@gmail.com>
2025-01-14 18:24:59 +0100

    feat: Create custom hook useUserPosts for fetching and filtering user-specific posts
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/hooks/useUserPosts.ts
@@ -0,0 +1,24 @@
+import useFetchRequest from "@/hooks/useFetchRequest";
+import { PostType } from "@/typings/types";
+
+const useUserPosts = (userId: string) => {
+  const {
+    data: posts,
+    error: postsError,
+    isFetching: arePostsFetching,
+    isLoading: arePostsLoading,
+  } = useFetchRequest(["posts"], `/api/posts`);
+
+  const blogPosts = Array.isArray(posts?.data)
+    ? posts.data
+        .filter((post: PostType) => post?.postedBy?._id.toString() === userId)
+        .sort(
+          (a: PostType, b: PostType) =>
+            new Date(String(b.createdAt ?? new Date())).getTime() -
+            new Date(String(a.createdAt ?? new Date())).getTime()
+        )
+    : [];
+  return { blogPosts, arePostsFetching, arePostsLoading, postsError };
+};
+
+export default useUserPosts;
