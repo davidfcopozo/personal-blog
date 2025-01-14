df08603a581d105f46f81ce48fcd18779bd842c3
David <davidfco.pozo@gmail.com>
2025-01-14 19:01:40 +0100

    fix: Update loader component import name in Login page for fixing build errors
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import BouncingSuaresLoader from "@/components/bouncing-squares-loader";
+import BouncingCirclesLoader from "@/components/bouncing-squares-loader";
 import { LoginForm } from "@/components/login-form";
 import { useAuth } from "@/context/AuthContext";
 
@@ -8,7 +8,7 @@ const Login = () => {
   const { isLoading } = useAuth();
   return (
     <div className="flex items-center justify-center mt-16 text-foreground min-h-screen">
-      {isLoading && <BouncingSuaresLoader />}
+      {isLoading && <BouncingCirclesLoader />}
       <LoginForm />
     </div>
   );
