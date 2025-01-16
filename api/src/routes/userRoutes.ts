77c6559343fdc0e4e0595206953df2f2bfdbc859
David <davidfco.pozo@gmail.com>
2025-01-16 17:57:46 +0100

    feat: Add route for image upload with user authentication
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -8,6 +8,7 @@ import {
   deleteUserById,
   toggleFollowUser,
   getUserByUsername,
+  uploadImage,
 } from "../controllers/userController";
 
 router.route("/").get(getUsers);
@@ -17,6 +18,8 @@ router
   .patch(auth, updateUserById)
   .put(auth, toggleFollowUser)
   .delete(auth, deleteUserById);
+router;
+router.route("/:id/images").post(auth, uploadImage);
 router.route("/username/:username").get(getUserByUsername);
 
 export default router;
