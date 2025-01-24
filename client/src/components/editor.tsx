273228933ccd7de80c80d051d9352bb668feb990
David <davidfco.pozo@gmail.com>
2025-01-24 18:16:51 +0100

    feat: Implement image upload functionality with modal in the editor component
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/editor.tsx
+++ client/src/components/editor.tsx
@@ -1,16 +1,31 @@
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import { formats, modules, REDO_ICON, UNDO_ICON } from "@/utils/blog-editor";
-import { EditorProps } from "@/typings/interfaces";
+import { EditorProps, ImageInterface } from "@/typings/interfaces";
+import { ImageUploadModal } from "./image-upload-modal";
 
 const Editor = ({ value, onChange, handleImageUpload }: EditorProps) => {
   const editorRef = useRef<ReactQuill>(null);
+  const [isImageUploadModalOpen, setImageUploadModalOpen] = useState(false);
+  const [uploadedImages, setUploadedImages] = useState<ImageInterface[]>([]);
 
-  let icons = Quill.import("ui/icons");
-  icons["undo"] = UNDO_ICON;
-  icons["redo"] = REDO_ICON;
+  const openImageUploadModal = useCallback(() => {
+    setImageUploadModalOpen((prev) => !prev);
+  }, []);
 
-  const imageHandler = useCallback(() => {
+  const handleInsertImage = useCallback(
+    (url: string) => {
+      if (editorRef.current) {
+        const quill = editorRef.current.getEditor();
+        const range = quill.getSelection(true);
+        quill.insertEmbed(range.index, "image", url);
+        quill.setSelection(range.index + 1, 1);
+      }
+    },
+    [editorRef]
+  );
+
+  const handleDirectImageUpload = useCallback(() => {
     const input = document.createElement("input");
     input.setAttribute("type", "file");
     input.setAttribute("accept", "image/*");
@@ -21,20 +36,17 @@ const Editor = ({ value, onChange, handleImageUpload }: EditorProps) => {
         const file = input.files[0];
         try {
           const url = await handleImageUpload(file);
-          if (editorRef && "current" in editorRef && editorRef.current) {
-            const quill = editorRef.current.getEditor();
-            if (quill) {
-              const range = quill.getSelection(true);
-              quill.insertEmbed(range.index, "image", url);
-              quill.setSelection(range.index + 1, 1);
-            }
-          }
+          handleInsertImage(url);
         } catch (error) {
           console.error("Error uploading image:", error);
         }
       }
     };
-  }, [handleImageUpload, editorRef]);
+  }, [handleImageUpload, handleInsertImage]);
+
+  let icons = Quill.import("ui/icons");
+  icons["undo"] = UNDO_ICON;
+  icons["redo"] = REDO_ICON;
 
   const undoHandler = useCallback(() => {
     if (editorRef && "current" in editorRef && editorRef.current) {
@@ -55,32 +67,42 @@ const Editor = ({ value, onChange, handleImageUpload }: EditorProps) => {
   useEffect(() => {
     if (editorRef.current) {
       const toolbar = editorRef.current.getEditor().getModule("toolbar");
-      toolbar.addHandler("image", imageHandler);
+      toolbar.addHandler("image", openImageUploadModal);
       toolbar.addHandler("undo", undoHandler);
       toolbar.addHandler("redu", redoHandler);
     }
-  }, [imageHandler, undoHandler, redoHandler]);
+  }, [openImageUploadModal, undoHandler, redoHandler]);
 
   return (
-    <ReactQuill
-      key="quil-editor"
-      ref={editorRef}
-      className="h-screen w-full"
-      formats={formats}
-      value={value}
-      onChange={onChange}
-      modules={{
-        ...modules,
-        toolbar: {
-          ...modules.toolbar,
-          handlers: {
-            image: imageHandler,
-            undo: undoHandler,
-            redo: redoHandler,
+    <>
+      <ReactQuill
+        key="quil-editor"
+        ref={editorRef}
+        className="h-screen w-full"
+        formats={formats}
+        value={value}
+        onChange={onChange}
+        modules={{
+          ...modules,
+          toolbar: {
+            ...modules.toolbar,
+            handlers: {
+              image: openImageUploadModal,
+              undo: undoHandler,
+              redo: redoHandler,
+            },
           },
-        },
-      }}
-    />
+        }}
+      />
+      <ImageUploadModal
+        isImageUploadModalOpen={isImageUploadModalOpen}
+        openImageUploadModal={openImageUploadModal}
+        onInsertImage={handleInsertImage}
+        handleImageUpload={handleImageUpload}
+        images={uploadedImages}
+        setImages={setUploadedImages}
+      />
+    </>
   );
 };
 
