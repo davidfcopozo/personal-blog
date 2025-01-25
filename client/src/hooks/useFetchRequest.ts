d3bd14b13411f6664e5b179f8737ea211776b55b
David <davidfco.pozo@gmail.com>
2025-01-25 15:38:25 +0100

    Revert to commit: Enhance CORS configuration for development and production environments, add utility for base URL, and update TypeScript settings
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/hooks/useFetchRequest.ts
+++ client/src/hooks/useFetchRequest.ts
@@ -1,19 +1,37 @@
-import apiClient from "@/utils/axiosIntance";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-
-const useFetchRequest = (queryKey: any[], url: string | null, options = {}) => {
-  return useQuery({
-    queryKey,
-    queryFn: async () => {
-      if (!url) return null;
-
-      const response = await apiClient.get(url);
-      return response.data;
-    },
-    enabled: !!url,
-    ...options,
-  });
+const getBaseURL = () => {
+  if (typeof window !== "undefined") {
+    return `http://${window.location.hostname}:3000`;
+  }
+  return "http://localhost:3000";
 };
+
+function useFetchRequest(queryKey: string[], url: string) {
+  const baseULR = getBaseURL();
+
+  try {
+    const fetchData = async () => {
+      const { data } = await axios.get(`${baseULR}${url}`);
+      return data;
+    };
+
+    const { data, error, isLoading, isFetching, refetch, isPending } = useQuery(
+      {
+        queryKey,
+        queryFn: fetchData,
+        refetchOnWindowFocus: false,
+        refetchOnMount: true,
+        gcTime: 0,
+        staleTime: 0,
+      }
+    );
+
+    return { data, error, isLoading, isFetching, isPending, refetch };
+  } catch (error: Error | any) {
+    throw new Error(error);
+  }
+}
 
 export default useFetchRequest;
