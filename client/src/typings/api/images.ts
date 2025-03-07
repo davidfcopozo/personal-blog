b87eac2060d302877f41c3da429aacaf92a6271d
David <davidfco.pozo@gmail.com>
2025-03-07 15:19:06 +0100

    feat: Add TypeScript typings for image API responses and payloads
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/typings/api/images.ts
@@ -0,0 +1,35 @@
+import { ImageInterface } from "../interfaces";
+
+export interface ImageUploadResponse {
+  success: boolean;
+  data: ImageInterface;
+  message?: string;
+}
+
+export interface ImageDeleteResponse {
+  success: boolean;
+  message: string;
+}
+
+export interface ImageUpdateResponse {
+  success: boolean;
+  data: ImageInterface;
+}
+
+export interface ImagesListResponse {
+  success: boolean;
+  data: ImageInterface[];
+}
+
+export interface ImageUploadPayload {
+  image: Omit<ImageInterface, "id"> & { hash: string };
+}
+
+export interface ImageDeletePayload {
+  imageId: string;
+}
+
+export interface ImageUpdatePayload {
+  imageId: string;
+  updates: Partial<ImageInterface>;
+}
