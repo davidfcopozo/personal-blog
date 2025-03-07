aa40da60e798d7be71d31fe58a5eb38459ffe802
David <davidfco.pozo@gmail.com>
2025-03-07 16:32:39 +0100

    fix: Remove unnecessary padding from QuillLoadingSkeleton component
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/quill-loading-skeleton.tsx
+++ client/src/components/quill-loading-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function QuillLoadingSkeleton() {
   return (
-    <div className="w-full h-screen rounded-lg bg-background px-4">
+    <div className="w-full h-screen rounded-lg bg-background">
       {/* Header/Title area */}
       <div className="border p-2 rounded-md mb-4 w-full">
         <Skeleton className="h-6 w-3/4" />
