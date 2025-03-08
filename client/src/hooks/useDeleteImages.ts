dc364259c6ceb17d215a98e2bc3dcca083efbbf8
David <davidfco.pozo@gmail.com>
2025-03-08 19:13:23 +0100

    fix: Changed hook name to useDeleteImages handling image deletions with improved API integration
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ client/src/hooks/useDeleteImages.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { UseMutationRequestProps } from "@/typings/interfaces";
+
+interface DeleteImageProps {
+  itemId: string;
+  key?: string;
+}
+
+const useDeleteImages = ({ url, onSuccess, onError, onMutate }: UseMutationRequestProps<any, any>) => {
+  const deleteImage = async ({ itemId }: DeleteImageProps) => {
+    const response = await axios.delete(`${url}?ids=${itemId}`);
+    return response.data;
+  };
+
+  return useMutation({
+    mutationFn: deleteImage,
+    onSuccess,
+    onError,
+    onMutate,
+  });
+};
+
+export default useDeleteImages;
