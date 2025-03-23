b7b0ca7073af7febc1ce1f34a4c51660fe2cac92
David <davidfco.pozo@gmail.com>
2025-03-23 20:04:38 +0100

    feat: Add route for verifying password reset tokens
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -10,6 +10,7 @@ import {
   resetPassword,
   logout,
   oAuth,
+  verifyResetToken,
 } from "../controllers/authController";
 
 router.route("/register").post(register);
@@ -19,6 +20,7 @@ router.route("/login").post(login);
 router.route("/oauth").post(oAuth);
 router.route("/forgot-password").post(forgotPassword);
 router.route("/reset-password").post(resetPassword);
+router.post("/verify-reset-token", verifyResetToken);
 router.route("/logout").get(auth, logout);
 
 export default router;
