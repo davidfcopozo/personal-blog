644afa47ab75f7f118db6b8e0263121ae20d9d4c
David <davidfco.pozo@gmail.com>
2025-03-24 22:06:29 +0100

    feat: Add SendPasswordChangedEmailProps interface for password change email
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/typings/utils/index.ts
+++ api/src/typings/utils/index.ts
@@ -11,6 +11,15 @@ export interface SendPasswordResetEmailProps {
   ip?: String | undefined;
 }
 
+export interface SendPasswordChangedEmailProps {
+  firstName: String;
+  email: String;
+  baseUrl: String;
+  proxyOrVPN?: Boolean | undefined;
+  geoLocation?: String | undefined;
+  ip?: String | undefined;
+}
+
 export interface SendVerificationEmailProps {
   firstName: String;
   email: String;
