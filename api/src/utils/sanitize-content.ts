8c4cc6b83217e5beec30e7c3e8a58fef68057102
David <davidfco.pozo@gmail.com>
2025-06-09 13:16:31 +0200

    feat: refactor DOMPurify initialization to use JSDOM for improved sanitization
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/utils/sanitize-content.ts
+++ api/src/utils/sanitize-content.ts
@@ -1,4 +1,8 @@
-import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
+import createDOMPurify from "dompurify";
+
+const window = new JSDOM("").window;
+const DOMPurify = createDOMPurify(window);
 
 const dompurifyConfig = {
   ALLOWED_TAGS: [
