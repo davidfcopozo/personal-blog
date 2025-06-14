a11bc26661db1c42289928e4db1121dd5f94316d
David <davidfco.pozo@gmail.com>
2025-06-14 13:10:42 +0200

    feat: enhance content sanitization to allow additional HTML tags and attributes for YouTube videos
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/utils/sanitize-content.ts
+++ client/src/utils/sanitize-content.ts
@@ -24,8 +24,17 @@ const dompurifyConfig = {
     "h4",
     "h5",
     "h6",
-  ],
-  ALLOWED_ATTR: [
+    "s", // Allow strikethrough
+    "del", // Allow deleted text
+    "div", // Allow div for YouTube videos
+    "iframe", // Allow iframe for YouTube videos
+    "table",
+    "thead",
+    "tbody",
+    "tr",
+    "td",
+    "th",
+  ],  ALLOWED_ATTR: [
     "href",
     "target",
     "alt",
@@ -33,16 +42,43 @@ const dompurifyConfig = {
     "src",
     "class",
     "data-language",
+    "data-youtube-video", // Allow YouTube video data attribute
+    "data-keep-ratio", // Allow image ratio data attribute
+    "data-src", // TipTap YouTube extension uses this
+    "data-start", // YouTube start time
+    "data-end", // YouTube end time
+    "data-aspect-ratio", // YouTube aspect ratio
     "spellcheck",
-  ],
-  ALLOW_DATA_ATTR: false, // Prevent data-* attributes unless explicitly needed
-  FORBID_TAGS: ["script", "iframe", "object", "embed", "style"], // Explicitly forbid script and style tags
-  FORBID_ATTR: ["on*", "style"], // Prevent event handler attributes
+    "width",
+    "height",
+    "frameborder",
+    "allowfullscreen",
+    "allow",
+    "style", // Allow style for width/height of videos
+    "loading", // For iframe loading attribute
+    "referrerpolicy", // For iframe security
+    "contenteditable", // Allow contenteditable for Tiptap
+    "draggable", // Allow draggable for Tiptap
+    "autoplay", // YouTube iframe attributes
+    "disablekbcontrols", // YouTube iframe attributes
+    "enableiframeapi", // YouTube iframe attributes
+    "endtime", // YouTube iframe attributes
+    "ivloadpolicy", // YouTube iframe attributes
+    "loop", // YouTube iframe attributes
+    "modestbranding", // YouTube iframe attributes
+    "origin", // YouTube iframe attributes
+    "playlist", // YouTube iframe attributes
+    "rel", // YouTube iframe attributes
+    "start", // YouTube iframe attributes
+  ],  ALLOW_DATA_ATTR: true, // Allow data-* attributes for video embeds
+  FORBID_TAGS: ["script", "object", "embed", "style"], // Remove iframe from forbidden tags
+  FORBID_ATTR: ["on*"], // Remove style from forbidden attributes - allow style for video dimensions
   ADD_ATTR: ["rel"], // Add rel attribute for links
-  /* ADD_TAGS: [], */ // Add custom tags if needed (e.g., embeds)
   RETURN_DOM: false, // Return HTML string
   RETURN_DOM_FRAGMENT: false, // Return string, not fragment
   KEEP_CONTENT: true, // Preserve inner content
+  // Allow YouTube iframe URLs only
+  ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|cid|xmpp|data):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
 };
 
 export const sanitizeContent = (dirtyHtml: string) => {
