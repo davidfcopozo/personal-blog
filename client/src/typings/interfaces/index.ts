24941f795297c1434061fbb2bec0c9684871b935
David <davidfco.pozo@gmail.com>
2025-06-17 12:27:24 +0200

    feat: add slug and onPreview properties to NewPostHeaderProps interface
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/typings/interfaces/index.ts
+++ client/src/typings/interfaces/index.ts
@@ -99,6 +99,8 @@ export interface NewPostHeaderProps {
   currentStatus?: "draft" | "published" | "unpublished";
   hasChanges?: boolean;
   isSaving?: boolean;
+  slug?: string;
+  onPreview?: () => void;
 }
 
 export interface BlogEditorProps {
