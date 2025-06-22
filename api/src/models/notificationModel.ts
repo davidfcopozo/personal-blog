fb4ac157b04930cd62a2e08ae91cbce42ab464bd
David <davidfco.pozo@gmail.com>
2025-06-22 12:36:26 +0200

    feat: create notification model with schema and indexing for efficient queries
    Signed-off-by: David <davidfco.pozo@gmail.com>

---
+++ api/src/models/notificationModel.ts
@@ -0,0 +1,60 @@
+import mongoose, { model } from "mongoose";
+import { NotificationInterface } from "../typings/models/notification";
+const { Schema } = mongoose;
+
+const notificationSchema = new Schema<NotificationInterface>(
+  {
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["mention", "comment", "reply", "bookmark", "like"],
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    relatedPost: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+    relatedComment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Index for efficient queries
+notificationSchema.index({ recipient: 1, createdAt: -1 });
+notificationSchema.index({ recipient: 1, isRead: 1 });
+
+// Populate sender information when querying
+notificationSchema.pre(
+  /^find/,
+  function (this: mongoose.Query<any, any>, next) {
+    this.populate({
+      path: "sender",
+      select: "firstName lastName username avatar",
+    });
+    next();
+  }
+);
+
+const Notification = model("Notification", notificationSchema);
+
+export default Notification;
