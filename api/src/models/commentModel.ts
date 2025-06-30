95d41260ded9e04b522536713fe346fe39adea19
David <davidfco.pozo@gmail.com>
2025-06-30 11:46:26 +0200

    feat: add likesCount virtual field and populateAnalytics method to Comment model
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/models/commentModel.ts
+++ api/src/models/commentModel.ts
@@ -18,11 +18,29 @@ const commentSchema = new mongoose.Schema<CommentInterface>(
     },
     parentId: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
     replies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Reply" }],
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+
     isReply: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
 );
+
+commentSchema.virtual("likesCount", {
+  ref: "CommentLike",
+  localField: "_id",
+  foreignField: "comment",
+  count: true,
+  match: { isActive: true },
+  justOne: false,
+});
+
+commentSchema.methods.populateAnalytics = async function () {
+  await this.populate("likesCount");
+  return this;
+};
+
+// Ensure virtual fields are serialized
+commentSchema.set("toJSON", { virtuals: true });
+commentSchema.set("toObject", { virtuals: true });
 
 const Comment = mongoose.model("Comment", commentSchema);
 
