d2212062c682016b90738fc429d2d22def4a7f9e
David <davidfco.pozo@gmail.com>
2025-06-30 18:01:21 +0200

    feat: add optional authentication to post routes for enhanced access control
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/routes/postRoutes.ts
+++ api/src/routes/postRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
 import { auth } from "../middleware/auth";
+import { optionalAuth } from "../middleware/optional-auth";
 import {
   createPost,
   getAllPosts,
@@ -15,16 +16,16 @@ import {
 } from "../controllers/postController";
 import { visitsCounter } from "../middleware/visits-counter";
 
-router.route("/").get(getAllPosts).post(auth, createPost);
+router.route("/").get(optionalAuth, getAllPosts).post(auth, createPost);
 router.route("/my-posts").get(auth, getUserPosts);
 router.route("/like").put(auth, toggleLike);
 router.route("/bookmark").put(auth, toggleBookmark);
 router.route("/preview/:slugOrId").get(auth, previewPost);
 router
   .route("/:slugOrId")
-  .get(visitsCounter, getPostBySlugOrId)
+  .get(optionalAuth, visitsCounter, getPostBySlugOrId)
   .patch(auth, updatePostBySlugOrId)
   .delete(auth, deletePostBySlugOrId);
-router.route("/category/:category").get(getPostsByCategory);
+router.route("/category/:category").get(optionalAuth, getPostsByCategory);
 
 export default router;
