ea67caee0048196158b0f5eaba39c6fea03ece43
David <davidfco.pozo@gmail.com>
2025-07-06 17:06:41 +0200

    feat: refactor EngagementAreaChart to use dynamic data from generateEngagementTimelineData
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/charts/engagement-area-chart.tsx
+++ client/src/components/charts/engagement-area-chart.tsx
@@ -8,26 +8,18 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import { PostType } from "@/typings/types";
+import { generateEngagementTimelineData } from "@/lib/analytics-utils";
 
-const data = [
-  { day: "Day 1", likes: 45, comments: 12, shares: 8 },
-  { day: "Day 2", likes: 52, comments: 15, shares: 10 },
-  { day: "Day 3", likes: 38, comments: 9, shares: 6 },
-  { day: "Day 4", likes: 67, comments: 18, shares: 12 },
-  { day: "Day 5", likes: 59, comments: 16, shares: 11 },
-  { day: "Day 6", likes: 78, comments: 22, shares: 15 },
-  { day: "Day 7", likes: 71, comments: 19, shares: 13 },
-  { day: "Day 8", likes: 89, comments: 25, shares: 17 },
-  { day: "Day 9", likes: 82, comments: 23, shares: 16 },
-  { day: "Day 10", likes: 95, comments: 28, shares: 19 },
-  { day: "Day 11", likes: 88, comments: 26, shares: 18 },
-  { day: "Day 12", likes: 103, comments: 31, shares: 21 },
-  { day: "Day 13", likes: 96, comments: 29, shares: 20 },
-  { day: "Day 14", likes: 112, comments: 34, shares: 23 },
-  { day: "Day 15", likes: 105, comments: 32, shares: 22 },
-];
+interface EngagementAreaChartProps {
+  blogPosts: PostType[];
+}
 
-export default function EngagementAreaChart() {
+export default function EngagementAreaChart({
+  blogPosts,
+}: EngagementAreaChartProps) {
+  const data = generateEngagementTimelineData(blogPosts);
+
   return (
     <ResponsiveContainer width="100%" height={300}>
       <AreaChart
